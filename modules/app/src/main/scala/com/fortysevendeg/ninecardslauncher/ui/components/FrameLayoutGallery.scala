1f5ec677d88b154238aa5ddc25471c11b380b482
Javi Pacheco <javielinux@gmail.com>
2015-03-12 16:57:24 +0100

    Gallery using one type of view

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/ui/components/FrameLayoutGallery.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/ui/components/FrameLayoutGallery.scala
@@ -1,19 +1,29 @@
 package com.fortysevendeg.ninecardslauncher.ui.components
 
+import android.animation.{Animator, AnimatorListenerAdapter, ObjectAnimator, ValueAnimator}
 import android.content.Context
+import android.os.Handler
 import android.support.v4.view.{MotionEventCompat, ViewConfigurationCompat}
 import android.util.AttributeSet
-import android.view.{MotionEvent, VelocityTracker, ViewConfiguration}
+import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams._
+import android.view.animation.DecelerateInterpolator
+import android.view.{MotionEvent, VelocityTracker, ViewConfiguration, ViewGroup}
 import android.widget.FrameLayout
+import com.fortysevendeg.macroid.extras.ViewGroupTweaks._
+import com.fortysevendeg.macroid.extras.ViewTweaks._
 import com.fortysevendeg.ninecardslauncher.ui.components.TouchState._
-import macroid.{AppContext, Tweak}
+import macroid.AppContext
+import macroid.FullDsl._
 
-abstract class FrameLayoutGallery(context: Context, attr: AttributeSet, defStyleAttr: Int)(implicit appContext: AppContext)
+abstract class FrameLayoutGallery[Holder <: ViewGroup, Data](context: Context, attr: AttributeSet, defStyleAttr: Int)(implicit appContext: AppContext)
   extends FrameLayout(context, attr, defStyleAttr) {
 
   def this(context: Context)(implicit appContext: AppContext) = this(context, null, 0)
 
   def this(context: Context, attr: AttributeSet)(implicit appContext: AppContext) = this(context, attr, 0)
+
+  val data: List[Data] = getData()
 
   var touchState = stopped
 
@@ -36,15 +46,193 @@ abstract class FrameLayoutGallery(context: Context, attr: AttributeSet, defStyle
       configuration.getScaledMinimumFlingVelocity)
   }
 
-  def isFirst: Boolean
+  val durationAnimation = appContext.get.getResources.getInteger(android.R.integer.config_shortAnimTime)
 
-  def isLast: Boolean
+  val mainAnimator: ObjectAnimator = new ObjectAnimator
 
-  def performScroll(delta: Float)
+  val hideAfterAnimationListener = new AnimatorListenerAdapter() {
+    override def onAnimationEnd(animation: Animator) {
+      new Handler().post(new Runnable() {
+        def run() = swapViews()
+      })
+      super.onAnimationEnd(animation)
+    }
+  }
 
-  def snap(velocity: Float)
+  var previousView = slot[Holder]
+  var nextView = slot[Holder]
+  var frontView = slot[Holder]
+  var displacement: Float = 0
 
-  def snapDestination()
+  var currentItem = 0
+
+  def getData(): List[Data]
+
+  def createView(): Holder
+
+  def populateView(view: Option[Holder], data: Data, position: Int)
+
+  createViews()
+
+  private def createViews() = {
+    val previous = createView()
+    val next = createView()
+    val front = createView()
+    previousView = Some(previous)
+    nextView = Some(next)
+    frontView = Some(front)
+    val params = new LayoutParams(MATCH_PARENT, MATCH_PARENT)
+    runUi(
+      (this <~ vgAddView(previous, params)) ~
+        (this <~ vgAddView(next, params)) ~
+        (this <~ vgAddView(front, params))
+    )
+    reset()
+  }
+
+  private def getSizeWidget = if (horizontalGallery) getWidth else getHeight
+
+  def isFirst: Boolean = currentItem == 0
+
+  def isLast: Boolean = currentItem == data.length - 1
+
+  def snap(velocity: Float): Unit = {
+    mainAnimator.cancel()
+    val destiny = velocity match {
+      case v if v > 0 && displacement > 0 => getSizeWidget
+      case v if v <= 0 && displacement < 0 => -getSizeWidget
+      case _ => 0
+    }
+    animateViews(destiny, durationAnimation)
+  }
+
+  def snapDestination(): Unit = {
+    val destiny = displacement match {
+      case d if d > getSizeWidget * .6f => getSizeWidget
+      case d if d < -getSizeWidget * .6f => -getSizeWidget
+      case _ => 0
+    }
+    animateViews(destiny, durationAnimation)
+    invalidate()
+  }
+
+  def performScroll(delta: Float): Unit = {
+    mainAnimator.removeAllListeners()
+    mainAnimator.cancel()
+    displacement = math.max(-getSizeWidget, Math.min(getSizeWidget, displacement - delta))
+    if (displacement > 0) {
+      runUi((previousView <~ vVisible) ~ (nextView <~ vGone))
+    } else {
+      runUi((previousView <~ vGone) ~ (nextView <~ vVisible))
+    }
+    applyTranslation(frontView, displacement)
+    transformPanelCanvas()
+  }
+
+  private def applyTranslation(view: Option[ViewGroup], translate: Float) =
+    runUi(view <~ (if (horizontalGallery) vTranslationX(translate) else vTranslationY(translate)))
+
+
+  private def transformPanelCanvas() = {
+    val percent = math.abs(displacement) / getSizeWidget
+    val fromLeft = displacement > 0
+    applyTransformer(if (fromLeft) previousView else nextView, percent, fromLeft)
+  }
+
+  private def applyTransformer(view: Option[ViewGroup], percent: Float, fromLeft: Boolean) = {
+    val ratio = (percent * .6f) + .4f
+    runUi(view <~ vAlpha(ratio) <~ vScaleX(ratio) <~ vScaleY(ratio))
+    val translate = {
+      val start = if (fromLeft) -(getSizeWidget * .4f) else getSizeWidget - (getSizeWidget * .4f)
+      start - (start * percent)
+    }
+    applyTranslation(view, translate)
+  }
+
+  private def animateViews(dest: Int, duration: Int) = {
+    mainAnimator.setFloatValues(displacement, dest)
+    mainAnimator.setDuration(duration)
+    if (dest != 0) {
+      mainAnimator.addListener(hideAfterAnimationListener)
+    } else {
+      mainAnimator.removeAllListeners()
+    }
+    mainAnimator.start()
+  }
+
+  private def next(): Unit = {
+    for {
+      front <- frontView
+      next <- nextView
+      previous <- previousView
+    } yield {
+      frontView = Some(next)
+      nextView = Some(previous)
+      previousView = Some(front)
+      currentItem = currentItem + 1
+      if (currentItem > data.size - 1) currentItem = 0
+    }
+  }
+
+  private def previous(): Unit = {
+    for {
+      front <- frontView
+      next <- nextView
+      previous <- previousView
+    } yield {
+      frontView = Some(previous)
+      nextView = Some(front)
+      previousView = Some(next)
+      currentItem = currentItem - 1
+      if (currentItem < 0) currentItem = data.length - 1
+    }
+  }
+
+  private def swapViews(): Unit = {
+    if (displacement < 0) next() else previous()
+    reset()
+  }
+
+  private def reset(): Unit = {
+    if (data.length > currentItem) {
+      populateView(frontView, data(currentItem), currentItem)
+
+      val positionLeft: Int = if (currentItem - 1 < 0) data.length - 1 else currentItem - 1
+      populateView(previousView, data(positionLeft), positionLeft)
+
+      val positionRight: Int = if (currentItem + 1 > data.length - 1) 0 else currentItem + 1
+      populateView(nextView, data(positionRight), positionRight)
+    }
+    displacement = 0
+    enabled = data.nonEmpty && data.length > 1
+
+    runUi(
+      (previousView <~ vGone) ~
+        (nextView <~ vGone <~ vBringToFront) ~
+        (frontView <~ vClearAnimation <~ vVisible <~ vBringToFront)
+    )
+
+    mainAnimator.removeAllListeners()
+    mainAnimator.cancel()
+
+    applyTranslation(frontView, displacement)
+    applyTranslation(nextView, getSizeWidget)
+    applyTranslation(previousView, -getSizeWidget)
+
+    frontView map {
+      front =>
+        mainAnimator.setTarget(front)
+        mainAnimator.setPropertyName(if (horizontalGallery) "translationX" else "translationY")
+        mainAnimator.setFloatValues(0, 0)
+        mainAnimator.setInterpolator(new DecelerateInterpolator())
+        mainAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+          override def onAnimationUpdate(arg0: ValueAnimator) {
+            displacement = arg0.getAnimatedValue.asInstanceOf[Float]
+            transformPanelCanvas()
+          }
+        })
+    }
+  }
 
   override def onInterceptTouchEvent(event: MotionEvent): Boolean = {
     super.onInterceptTouchEvent(event)
@@ -102,57 +290,31 @@ abstract class FrameLayoutGallery(context: Context, attr: AttributeSet, defStyle
     val yMoved = yDiff > touchSlop
 
     if (xMoved || yMoved) {
-      if (infinite) {
-        if (horizontalGallery && xDiff > yDiff) {
-          touchState = scrolling
-        } else if (!horizontalGallery && xDiff < yDiff) {
-          touchState = scrolling
-        }
-      } else {
-        if (horizontalGallery && xDiff > yDiff && x - lastMotionX > 0 && !isFirst) {
-          // swipe to right
-          touchState = scrolling
-        } else if (horizontalGallery && xDiff > yDiff && x - lastMotionX < 0 && !isLast) {
-          // swipe to left
-          touchState = scrolling
-        } else if (!horizontalGallery && xDiff < yDiff && y - lastMotionY > 0 && !isFirst) {
-          // swipe to down
-          touchState = scrolling
-        } else if (!horizontalGallery && xDiff < yDiff && y - lastMotionY < 0 && !isLast) {
-          // swipe to up
-          touchState = scrolling
-        }
+      val isScrolling = (infinite, horizontalGallery, xDiff > yDiff) match {
+        case (i, h, xMove) if i && h && xMove => true
+        case (i, h, xMove) if i && !h && !xMove => true
+        case (i, h, xMove) if !i && h && xMove && x - lastMotionX > 0 && !isFirst => true
+        case (i, h, xMove) if !i && h && xMove && x - lastMotionX < 0 && !isLast => true
+        case (i, h, xMove) if !i && !h && !xMove && y - lastMotionY > 0 && !isFirst => true
+        case (i, h, xMove) if !i && !h && !xMove && y - lastMotionY < 0 && !isLast => true
+        case _ => false
       }
+      if (isScrolling) touchState = scrolling
       lastMotionX = x
       lastMotionY = y
     }
   }
 
-  private def computeFling() {
-    velocityTracker map {
-      tracker =>
-        tracker.computeCurrentVelocity(1000, maximumVelocity)
-        val velocity = if (horizontalGallery) tracker.getXVelocity else tracker.getYVelocity
-        if (touchState == scrolling) {
-          if (math.abs(velocity) > minimumVelocity) {
-            snap(velocity)
-          } else {
-            snapDestination()
-          }
-        }
-        tracker.recycle()
-        velocityTracker = None
-    }
+  private def computeFling() = velocityTracker map {
+    tracker =>
+      tracker.computeCurrentVelocity(1000, maximumVelocity)
+      val velocity = if (horizontalGallery) tracker.getXVelocity else tracker.getYVelocity
+      if (touchState == scrolling) {
+        if (math.abs(velocity) > minimumVelocity) snap(velocity) else snapDestination()
+      }
+      tracker.recycle()
+      velocityTracker = None
   }
-
-}
-
-object FrameLayoutGallery {
-  type W = FrameLayoutGallery
-
-  def flgEnabled(e: Boolean): Tweak[W] = Tweak[W](_.enabled = e)
-  def flgHorizontalGallery(h: Boolean): Tweak[W] = Tweak[W](_.horizontalGallery = h)
-  def flgInfinite(i: Boolean): Tweak[W] = Tweak[W](_.infinite = i)
 
 }
 
--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/ui/components/TestGallery.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/ui/components/TestGallery.scala
@@ -1,218 +1,22 @@
 package com.fortysevendeg.ninecardslauncher.ui.components
 
-import android.animation.{ValueAnimator, Animator, AnimatorListenerAdapter, ObjectAnimator}
 import android.content.Context
 import android.graphics.Color
-import android.os.Handler
-import android.util.AttributeSet
-import android.view.{Gravity, ViewGroup}
-import android.view.ViewGroup.LayoutParams
-import android.view.ViewGroup.LayoutParams._
-import android.view.animation.DecelerateInterpolator
-import com.fortysevendeg.macroid.extras.ViewTweaks._
-import com.fortysevendeg.macroid.extras.ViewGroupTweaks._
-import com.fortysevendeg.macroid.extras.TextTweaks._
+import android.view.Gravity
 import android.widget.{LinearLayout, TextView}
-import macroid.{ActivityContext, AppContext}
+import com.fortysevendeg.macroid.extras.TextTweaks._
+import com.fortysevendeg.macroid.extras.ViewTweaks._
 import macroid.FullDsl._
-import FrameLayoutGallery._
+import macroid.{Tweak, ActivityContext, AppContext}
 
-class TestGallery(context: Context, attr: AttributeSet, defStyleAttr: Int)(implicit appContext: AppContext, activityContext: ActivityContext)
-  extends FrameLayoutGallery(context, attr, defStyleAttr) {
+class TestGallery(context: Context)(implicit appContext: AppContext, activityContext: ActivityContext)
+  extends FrameLayoutGallery[TestGalleryHolder, String](context, null, 0) {
 
-  def this(context: Context)(implicit appContext: AppContext, activityContext: ActivityContext) = this(context, null, 0)
+  override def getData(): List[String] = List("1", "2", "3", "4", "5", "6")
 
-  def this(context: Context, attr: AttributeSet)(implicit appContext: AppContext, activityContext: ActivityContext) = this(context, attr, 0)
+  override def createView(): TestGalleryHolder = new TestGalleryHolder
 
-  val durationAnimation = appContext.get.getResources.getInteger(android.R.integer.config_shortAnimTime)
-
-  val mainAnimator: ObjectAnimator = new ObjectAnimator
-
-  val hideAfterAnimationListener = new AnimatorListenerAdapter() {
-    override def onAnimationEnd(animation: Animator) {
-      new Handler().postDelayed(new Runnable() {
-        def run() {
-          swapViews()
-        }
-      }, 1)
-      super.onAnimationEnd(animation)
-    }
-  }
-
-  var previousView = slot[TestGalleryHolder]
-  var nextView = slot[TestGalleryHolder]
-  var frontView = slot[TestGalleryHolder]
-  var displacement: Float = 0
-
-  var data = List("1", "2", "3", "4", "5", "6")
-
-  var currentItem = 0
-
-  createViews()
-
-  private def createViews() = {
-    val previous = new TestGalleryHolder(Color.RED)
-    val next = new TestGalleryHolder(Color.GREEN)
-    val front = new TestGalleryHolder(Color.BLUE)
-    previousView = Some(previous)
-    nextView = Some(next)
-    frontView = Some(front)
-    val params = new LayoutParams(MATCH_PARENT, MATCH_PARENT)
-    runUi(
-      (this <~ vgAddView(previous, params)) ~
-        (this <~ vgAddView(next, params)) ~
-        (this <~ vgAddView(front, params))
-    )
-    reset()
-  }
-
-  private def getSizeWidget = if (horizontalGallery) getWidth else getHeight
-
-  override def isFirst: Boolean = currentItem == 0
-
-  override def isLast: Boolean = currentItem == data.length - 1
-
-  override def snap(velocity: Float): Unit = {
-    mainAnimator.cancel()
-    val destiny = velocity match {
-      case v if v > 0 && displacement > 0 => getSizeWidget
-      case v if v <= 0 && displacement < 0 => -getSizeWidget
-      case _ => 0
-    }
-    animateViews(destiny, durationAnimation)
-  }
-
-  override def snapDestination(): Unit = {
-    val destiny = displacement match {
-      case d if d > getSizeWidget * .6f => getSizeWidget
-      case d if d < -getSizeWidget * .6f => -getSizeWidget
-      case _ => 0
-    }
-    animateViews(destiny, durationAnimation)
-    invalidate()
-  }
-
-  override def performScroll(delta: Float): Unit = {
-    mainAnimator.removeAllListeners()
-    mainAnimator.cancel()
-    displacement = math.max(-getSizeWidget, Math.min(getSizeWidget, displacement - delta))
-    if (displacement > 0) {
-      runUi((previousView <~ vVisible) ~ (nextView <~ vGone))
-    } else {
-      runUi((previousView <~ vGone) ~ (nextView <~ vVisible))
-    }
-    applyTranslation(frontView, displacement)
-    transformPanelCanvas()
-  }
-
-  private def applyTranslation(view: Option[ViewGroup], translate: Float) =
-    runUi(view <~ (if (horizontalGallery) vTranslationX(translate) else vTranslationY(translate)))
-
-
-  private def transformPanelCanvas() = {
-    val percent = math.abs(displacement) / getSizeWidget
-    val fromLeft = displacement > 0
-    applyTransformer(if (fromLeft) previousView else nextView, percent, fromLeft)
-  }
-
-  private def applyTransformer(view: Option[ViewGroup], percent: Float, fromLeft: Boolean) = {
-    val ratio = (percent * .6f) + .4f
-    runUi(
-      view <~ vAlpha(ratio) <~ vScaleX(ratio) <~ vScaleY(ratio)
-    )
-    val translate = {
-      val start = if (fromLeft) -(getSizeWidget * .4f) else getSizeWidget - (getSizeWidget * .4f)
-      start - (start * percent)
-    }
-    applyTranslation(view, translate)
-  }
-
-  private def animateViews(dest: Int, duration: Int) = {
-    mainAnimator.setFloatValues(displacement, dest)
-    mainAnimator.setDuration(duration)
-    if (dest != 0) {
-      mainAnimator.addListener(hideAfterAnimationListener)
-    } else {
-      mainAnimator.removeAllListeners()
-    }
-    mainAnimator.start()
-  }
-
-  private def next(): Unit = {
-    for {
-      front <- frontView
-      next <- nextView
-      previous <- previousView
-    } yield {
-      frontView = Some(next)
-      nextView = Some(previous)
-      previousView = Some(front)
-      currentItem = currentItem + 1
-      if (currentItem > data.size - 1) currentItem = 0
-    }
-  }
-
-  private def previous(): Unit = {
-    for {
-      front <- frontView
-      next <- nextView
-      previous <- previousView
-    } yield {
-      frontView = Some(previous)
-      nextView = Some(front)
-      previousView = Some(next)
-      currentItem = currentItem - 1
-      if (currentItem < 0) currentItem = data.length - 1
-    }
-  }
-
-  private def swapViews(): Unit = {
-    if (displacement < 0) next() else previous()
-    reset()
-  }
-
-  private def reset(): Unit = {
-    if (data.length > currentItem) {
-      populateView(frontView, data(currentItem), currentItem)
-
-      val positionLeft: Int = if (currentItem - 1 < 0) data.length - 1 else currentItem - 1
-      populateView(previousView, data(positionLeft), positionLeft)
-
-      val positionRight: Int = if (currentItem + 1 > data.length - 1) 0 else currentItem + 1
-      populateView(nextView, data(positionRight), positionRight)
-    }
-    displacement = 0
-
-    runUi(
-      (this <~ flgEnabled(data.nonEmpty && data.length > 1)) ~
-        (frontView <~ vClearAnimation <~ vVisible <~ vBringToFront) ~
-        (nextView <~ vGone <~ vBringToFront) ~
-        (previousView <~ vGone)
-    )
-
-    mainAnimator.removeAllListeners()
-    mainAnimator.cancel()
-
-    applyTranslation(frontView, displacement)
-    applyTranslation(nextView, getSizeWidget)
-    applyTranslation(previousView, -getSizeWidget)
-
-    frontView map {
-      front =>
-        mainAnimator.setTarget(front)
-        mainAnimator.setPropertyName(if (horizontalGallery) "translationX" else "translationY")
-        mainAnimator.setFloatValues(0, 0)
-        mainAnimator.setInterpolator(new DecelerateInterpolator())
-        mainAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-          override def onAnimationUpdate(arg0: ValueAnimator) {
-            displacement = arg0.getAnimatedValue.asInstanceOf[Float]
-            transformPanelCanvas()
-          }
-        })
-    }
-  }
-
-  private def populateView(view: Option[TestGalleryHolder], data: String, position: Int) = {
+  override def populateView(view: Option[TestGalleryHolder], data: String, position: Int) = {
     view map {
       v =>
         runUi(v.text <~ tvText(data))
@@ -221,15 +25,26 @@ class TestGallery(context: Context, attr: AttributeSet, defStyleAttr: Int)(impli
 
 }
 
-class TestGalleryHolder(color: Int)(implicit appContext: AppContext, activityContext: ActivityContext)
+class TestGalleryHolder(implicit appContext: AppContext, activityContext: ActivityContext)
   extends LinearLayout(activityContext.get) {
 
   var text = slot[TextView]
 
   addView(
     getUi(
-      w[TextView] <~ wire(text) <~ vMatchParent <~ tvSize(80) <~ vBackgroundColor(color) <~ tvColor(Color.WHITE) <~ tvText("-") <~ tvGravity(Gravity.CENTER)
+      w[TextView] <~ wire(text) <~ vMatchParent <~ tvSize(80) <~ tvColor(Color.WHITE) <~ tvText("-") <~ tvGravity(Gravity.CENTER)
     )
   )
+
+}
+
+object TestGalleryTweaks {
+  type W = TestGallery
+
+  def flgEnabled(e: Boolean): Tweak[W] = Tweak[W](_.enabled = e)
+
+  def flgHorizontalGallery(h: Boolean): Tweak[W] = Tweak[W](_.horizontalGallery = h)
+
+  def flgInfinite(i: Boolean): Tweak[W] = Tweak[W](_.infinite = i)
 
 }
