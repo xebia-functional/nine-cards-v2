5b924ab7c79b6f19e40de05faae5ff2dbc84e770
franciscodr <francisco.d@47deg.com>
2015-03-26 18:29:00 +0100

    9C-96 Adds Content Provider implementation

---
+++ modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/provider/DBUtils.scala
@@ -0,0 +1,57 @@
+package com.fortysevendeg.ninecardslauncher.provider
+
+import android.content.Context
+import android.database.Cursor
+import macroid.AppContext
+import scala.annotation.tailrec
+
+trait DBUtils {
+
+  def emptyAllTables(implicit appContext: AppContext) = {
+    val resolver = appContext.get.getContentResolver
+    resolver.delete(NineCardsContentProvider.ContentUriCacheCategory, "", Array.empty)
+    resolver.delete(NineCardsContentProvider.ContentUriCard, "", Array.empty)
+    resolver.delete(NineCardsContentProvider.ContentUriCollection, "", Array.empty)
+    resolver.delete(NineCardsContentProvider.ContentUriGeoInfo, "", Array.empty)
+  }
+
+  def execAllVersionsDB(context: Context) =
+    for (i <- 1 to NineCardsSqlHelper.DatabaseVersion) execVersion(context, i)
+
+  def execVersionsDB(context: Context, oldVersion: Int, newVersion: Int) {
+    for (i <- oldVersion + 1 to newVersion) execVersion(context, i)
+  }
+
+  def execVersion(context: Context, version: Int) {}
+
+  def getEntityFromCursor[T](cursor: Option[Cursor], conversionFunction: Cursor => T): Option[T] = cursor match {
+    case Some(cursorObject) if cursorObject.moveToFirst() =>
+      val result = Some(conversionFunction(cursorObject))
+      cursorObject.close()
+      result
+    case _ => None
+  }
+
+  def getListFromCursor[T](cursor: Option[Cursor], conversionFunction: Cursor => T): Seq[T] = {
+    @tailrec
+    def getListFromEntityLoop(cursor: Cursor, result: Seq[T]): Seq[T] = {
+      cursor match {
+        case validCursor if validCursor.isAfterLast => result
+        case _ => {
+          val entity = conversionFunction(cursor)
+          cursor.moveToNext
+          getListFromEntityLoop(cursor, entity +: result)
+        }
+      }
+    }
+
+    cursor match {
+      case Some(cursorObject) if cursorObject.moveToFirst() => {
+        val result = getListFromEntityLoop(cursorObject, Seq.empty[T])
+        cursorObject.close()
+        result
+      }
+      case _ => Seq.empty[T]
+    }
+  }
+}
---
+++ modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/provider/NineCardsContentProvider.scala
@@ -0,0 +1,150 @@
+package com.fortysevendeg.ninecardslauncher.provider
+
+import android.content.{ContentUris, UriMatcher, ContentValues, ContentProvider}
+import android.database.Cursor
+import android.database.sqlite.{SQLiteQueryBuilder, SQLiteDatabase}
+import android.net.Uri
+
+import NineCardsContentProvider._
+
+class NineCardsContentProvider extends ContentProvider {
+
+  lazy val nineCardsSqlHelper = new NineCardsSqlHelper(getContext)
+  lazy val database: Option[SQLiteDatabase] = Option[SQLiteDatabase](nineCardsSqlHelper.getWritableDatabase)
+
+  private def getUriInfo(uri: Uri): (String, MimeType) = uriMatcher.`match`(uri) match {
+    case code if code == CodeCacheCategoryAllItems => (CacheCategoryEntity.Table, MimeTypeAllItems)
+    case code if code == CodeCacheCategorySingleItem => (CacheCategoryEntity.Table, MimeTypeSingleItem)
+    case code if code == CodeCardAllItems => (CardEntity.Table, MimeTypeAllItems)
+    case code if code == CodeCardSingleItem => (CardEntity.Table, MimeTypeSingleItem)
+    case code if code == CodeCollectionAllItems => (CollectionEntity.Table, MimeTypeAllItems)
+    case code if code == CodeCollectionSingleItem => (CollectionEntity.Table, MimeTypeSingleItem)
+    case code if code == CodeGeoInfoAllItems => (GeoInfoEntity.Table, MimeTypeAllItems)
+    case code if code == CodeGeoInfoSingleItem => (GeoInfoEntity.Table, MimeTypeSingleItem)
+    case _ => throw new IllegalArgumentException(InvalidUri + uri)
+  }
+
+  override def onCreate(): Boolean = database match {
+    case Some(databaseObject) if databaseObject.isOpen => true
+    case _ => false
+  }
+
+  override def onLowMemory() {
+    super.onLowMemory()
+    nineCardsSqlHelper.close()
+  }
+
+  override def getType(uri: Uri): String =  {
+    getUriInfo(uri) match {
+      case (_, MimeTypeAllItems) => MimeTypeAllItemsValue
+      case (_, MimeTypeSingleItem) => MimeTypeSingleItemValue
+    }
+  }
+
+  override def update(uri: Uri, values: ContentValues, selection: String, selectionArgs: Array[String]): Int = {
+    val (tableName, mimeType) = getUriInfo(uri)
+
+    mimeType match {
+      case MimeTypeSingleItem =>
+        getOrOpenDatabase.update(
+          tableName,
+          values,
+          s"${NineCardsSqlHelper.Id} = ?",
+          Seq(uri.getPathSegments.get(1)).toArray)
+      case MimeTypeAllItems =>
+        getOrOpenDatabase.update(tableName, values, selection, selectionArgs)
+    }
+  }
+
+  override def insert(uri: Uri, values: ContentValues): Uri = {
+    val (tableName, mimeType) = getUriInfo(uri)
+
+    mimeType match {
+      case MimeTypeAllItems =>
+        ContentUris.withAppendedId(
+          ContentUriCacheCategory,
+          getOrOpenDatabase.insert(tableName, NineCardsSqlHelper.DatabaseName, values))
+      case _ => throw new IllegalArgumentException(InvalidUri + uri)
+    }
+  }
+
+  override def delete(uri: Uri, selection: String, selectionArgs: Array[String]): Int = {
+    val (tableName, mimeType) = getUriInfo(uri)
+
+    mimeType match {
+      case MimeTypeSingleItem =>
+        getOrOpenDatabase.delete(
+          tableName,
+          s"${NineCardsSqlHelper.Id} = ?",
+          Seq(uri.getPathSegments.get(1)).toArray)
+      case MimeTypeAllItems =>
+        getOrOpenDatabase.delete(tableName, selection, selectionArgs)
+    }
+  }
+
+  override def query(
+      uri: Uri,
+      projection: Array[String],
+      selection: String,
+      selectionArgs: Array[String],
+      sortOrder: String): Cursor = {
+    val (tableName, mimeType) = getUriInfo(uri)
+
+    mimeType match {
+      case MimeTypeSingleItem =>
+        val queryBuilder = new SQLiteQueryBuilder()
+        queryBuilder.setTables(tableName)
+        queryBuilder.query(
+          getOrOpenDatabase,
+          projection,
+          s"${NineCardsSqlHelper.Id} = ?",
+          Seq(uri.getPathSegments.get(1)).toArray,
+          null,
+          null,
+          sortOrder)
+      case MimeTypeAllItems =>
+        val queryBuilder = new SQLiteQueryBuilder()
+        queryBuilder.setTables(tableName)
+        queryBuilder.query(getOrOpenDatabase, projection, selection, selectionArgs, null, null, sortOrder)
+    }
+  }
+
+  private def getOrOpenDatabase = database match {
+    case Some(databaseObject) if databaseObject.isOpen => databaseObject
+    case _ => nineCardsSqlHelper.getWritableDatabase
+  }
+}
+
+object NineCardsContentProvider {
+  val InvalidUri = "Invalid uri: "
+  val AuthorityPart = "com.fortysevendeg.ninecardslauncher"
+  val ContentPrefix = "content://"
+  val ContentUriCacheCategory = Uri.parse(s"$ContentPrefix$AuthorityPart/${CacheCategoryEntity.Table}")
+  val CodeCacheCategoryAllItems = 1
+  val CodeCacheCategorySingleItem = 2
+  val ContentUriCard = Uri.parse(s"$ContentPrefix$AuthorityPart/${CardEntity.Table}")
+  val CodeCardAllItems = 3
+  val CodeCardSingleItem = 4
+  val ContentUriCollection = Uri.parse(s"$ContentPrefix$AuthorityPart/${CollectionEntity.Table}")
+  val CodeCollectionAllItems = 5
+  val CodeCollectionSingleItem = 6
+  val ContentUriGeoInfo = Uri.parse(s"$ContentPrefix$AuthorityPart/${GeoInfoEntity.Table}")
+  val CodeGeoInfoAllItems = 7
+  val CodeGeoInfoSingleItem = 8
+  val MimeTypeAllItemsValue = "vnd.android.cursor.dir/vnd.com.fortysevendeg.ninecardslauncher"
+  val MimeTypeSingleItemValue = "vnd.android.cursor.item/vnd.com.fortysevendeg.ninecardslauncher"
+
+  val uriMatcher = new UriMatcher(UriMatcher.NO_MATCH)
+  uriMatcher.addURI(AuthorityPart, CacheCategoryEntity.Table, CodeCacheCategoryAllItems)
+  uriMatcher.addURI(AuthorityPart, s"${CacheCategoryEntity.Table}/#", CodeCacheCategorySingleItem)
+  uriMatcher.addURI(AuthorityPart, CardEntity.Table, CodeCardAllItems)
+  uriMatcher.addURI(AuthorityPart, s"${CardEntity.Table}/#", CodeCardSingleItem)
+  uriMatcher.addURI(AuthorityPart, CollectionEntity.Table, CodeCollectionAllItems)
+  uriMatcher.addURI(AuthorityPart, s"${CollectionEntity.Table}/#", CodeCollectionSingleItem)
+  uriMatcher.addURI(AuthorityPart, GeoInfoEntity.Table, CodeGeoInfoAllItems)
+  uriMatcher.addURI(AuthorityPart, s"${GeoInfoEntity.Table}/#", CodeGeoInfoSingleItem)
+}
+
+sealed trait MimeType
+object MimeTypeAllItems extends MimeType
+object MimeTypeSingleItem extends MimeType
