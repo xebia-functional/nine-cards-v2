ad9d19bd39f6b59c2fd683cfad3cc2de1ce479db
franciscodr <francisco.d@47deg.com>
2015-04-07 17:05:12 +0200

    9C-96: Adds Seq[Card] into Collection repository case class

--- modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/model/Collection.scala
+++ modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/model/Collection.scala
@@ -7,8 +7,22 @@ case class Collection(
     `type`: String,
     icon: String,
     themedColorIndex: Int,
-    appsCategory: String,
-    constrains: String,
-    originalSharedCollectionId: String,
-    sharedCollectionId: String,
-    sharedCollectionSubscribed: Boolean)
+    appsCategory: Option[String] = None,
+    constrains: Option[String] = None,
+    originalSharedCollectionId: Option[String] = None,
+    sharedCollectionId: Option[String] = None,
+    sharedCollectionSubscribed: Boolean,
+    cards: Seq[Card])
+
+case class Card(
+    id: Int,
+    position: Int,
+    micros: Int = 0,
+    term: String,
+    packageName: Option[String],
+    `type`: String,
+    intent: String,
+    imagePath: String,
+    starRating: Option[Double] = None,
+    numDownloads: Option[String] = None,
+    notification: Option[String] = None)
