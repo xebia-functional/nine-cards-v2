14b3a04d609d1d87e28f6e9256ba4a605d4b4459
franciscodr <francisco.d@47deg.com>
2015-04-07 16:46:38 +0200

    9C-96: Adds new dependencies into NineCardRepositoryClient

--- modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/NineCardRepositoryClient.scala
+++ modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/NineCardRepositoryClient.scala
@@ -1,8 +1,9 @@
 package com.fortysevendeg.ninecardslauncher.repository
 
-import com.fortysevendeg.ninecardslauncher.repository.repositories.CollectionRepositoryClient
 import com.fortysevendeg.macroid.extras.AppContextProvider
+import com.fortysevendeg.ninecardslauncher.repository.repositories.{CardRepositoryClient, CollectionRepositoryClient}
 
 trait NineCardRepositoryClient
     extends CollectionRepositoryClient
+    with CardRepositoryClient
     with AppContextProvider
