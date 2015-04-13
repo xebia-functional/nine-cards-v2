6b415c991043b723ccd5747c76a7d3a4db4cc692
Fede Fernández <federico.f.b@47deg.com>
2015-04-13 19:02:30 +0200

    Changes global ExecutionContext implicit to method specific

--- modules/api/src/main/scala/com/fortysevendeg/rest/client/http/HttpClient.scala
+++ modules/api/src/main/scala/com/fortysevendeg/rest/client/http/HttpClient.scala
@@ -2,21 +2,21 @@ package com.fortysevendeg.rest.client.http
 
 import play.api.libs.json.Writes
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.reflect.runtime.universe.TypeTag
 
 trait HttpClient {
 
-  def doGet(url: String, httpHeaders: Seq[(String, String)]): Future[HttpClientResponse]
+  def doGet(url: String, httpHeaders: Seq[(String, String)])(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
-  def doDelete(url: String, httpHeaders: Seq[(String, String)]): Future[HttpClientResponse]
+  def doDelete(url: String, httpHeaders: Seq[(String, String)])(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
-  def doPost(url: String, httpHeaders: Seq[(String, String)]): Future[HttpClientResponse]
+  def doPost(url: String, httpHeaders: Seq[(String, String)])(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
-  def doPost[Req: TypeTag: Writes](url: String, httpHeaders: Seq[(String, String)], body: Req): Future[HttpClientResponse]
+  def doPost[Req: TypeTag: Writes](url: String, httpHeaders: Seq[(String, String)], body: Req)(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
-  def doPut(url: String, httpHeaders: Seq[(String, String)]): Future[HttpClientResponse]
+  def doPut(url: String, httpHeaders: Seq[(String, String)])(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
-  def doPut[Req: TypeTag: Writes](url: String, httpHeaders: Seq[(String, String)], body: Req): Future[HttpClientResponse]
+  def doPut[Req: TypeTag: Writes](url: String, httpHeaders: Seq[(String, String)], body: Req)(implicit executionContext: ExecutionContext): Future[HttpClientResponse]
 
 }
