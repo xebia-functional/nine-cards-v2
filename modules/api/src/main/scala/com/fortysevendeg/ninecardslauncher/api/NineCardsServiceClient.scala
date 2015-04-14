760f837d22ecb00baca4408e785bbfa8c1c7aedf
Fede Fernández <federico.f.b@47deg.com>
2015-04-14 17:55:27 +0200

    Adds test comment

--- modules/api/src/main/scala/com/fortysevendeg/ninecardslauncher/api/NineCardsServiceClient.scala
+++ modules/api/src/main/scala/com/fortysevendeg/ninecardslauncher/api/NineCardsServiceClient.scala
@@ -3,6 +3,9 @@ package com.fortysevendeg.ninecardslauncher.api
 import com.fortysevendeg.ninecardslauncher.api.services.{UserConfigServiceClient, SharedCollectionsServiceClient}
 import com.fortysevendeg.rest.client.ServiceClient
 
+/*
+ * Test comment
+ */
 trait NineCardsServiceClient
   extends ServiceClient
   with UserConfigServiceClient
