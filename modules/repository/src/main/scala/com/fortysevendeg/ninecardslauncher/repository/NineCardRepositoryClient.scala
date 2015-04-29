b59415545c3d6a2ad3133dbaba275b5fb267edf7
franciscodr <francisco.d@47deg.com>
2015-04-29 10:17:12 +0200

    9C-98: Adds new repositories into NineCardRepositoryClient

--- modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/NineCardRepositoryClient.scala
+++ modules/repository/src/main/scala/com/fortysevendeg/ninecardslauncher/repository/NineCardRepositoryClient.scala
@@ -1,9 +1,11 @@
 package com.fortysevendeg.ninecardslauncher.repository
 
 import com.fortysevendeg.ninecardslauncher.commons.ContentResolverProvider
-import com.fortysevendeg.ninecardslauncher.repository.repositories.{CardRepositoryClient, CollectionRepositoryClient}
+import com.fortysevendeg.ninecardslauncher.repository.repositories._
 
 trait NineCardRepositoryClient
-    extends CardRepositoryClient
+    extends CacheCategoryRepositoryClient
+    with CardRepositoryClient
     with CollectionRepositoryClient
+    with GeoInfoRepositoryClient
     with ContentResolverProvider
