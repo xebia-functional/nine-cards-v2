725e099bd56ad93c8572132cf45ab85d318c2df7
Fede Fernández <federico.f.b@47deg.com>
2015-05-28 09:30:56 +0200

    Improves the import for R.color

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/modules/theme/Theme.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/modules/theme/Theme.scala
@@ -1,7 +1,7 @@
 package com.fortysevendeg.ninecardslauncher.modules.theme
 
 import com.fortysevendeg.macroid.extras.ResourcesExtras._
-import com.fortysevendeg.ninecardslauncher2.R
+import com.fortysevendeg.ninecardslauncher2.R.color._
 import macroid.ContextWrapper
 
 sealed trait Theme {
@@ -54,52 +54,52 @@ sealed trait Theme {
 
 case object ThemeDark extends Theme {
 
-  override val searchBackgroundColorResource: Int = R.color.search_background_tint_dark
+  override val searchBackgroundColorResource: Int = search_background_tint_dark
 
-  override val searchPressedColorResource: Int = R.color.search_press_tint_dark
+  override val searchPressedColorResource: Int = search_press_tint_dark
 
-  override val searchGoogleColorResource: Int = R.color.search_google_tint_dark
+  override val searchGoogleColorResource: Int = search_google_tint_dark
 
-  override val searchIconsColorResource: Int = R.color.search_icons_tint_dark
+  override val searchIconsColorResource: Int = search_icons_tint_dark
 
-  override val appDrawerPressedColorResource: Int = R.color.app_drawer_press_tint_dark
+  override val appDrawerPressedColorResource: Int = app_drawer_press_tint_dark
 
-  override val collectionDetailBackgroundColorResource: Int = R.color.collection_detail_background_dark
+  override val collectionDetailBackgroundColorResource: Int = collection_detail_background_dark
 
-  override val collectionDetailTextCardColorResource: Int = R.color.collection_detail_text_card_dark
+  override val collectionDetailTextCardColorResource: Int = collection_detail_text_card_dark
 
-  override val collectionDetailCardBackgroundColorResource: Int = R.color.collection_detail_background_card_dark
+  override val collectionDetailCardBackgroundColorResource: Int = collection_detail_background_card_dark
 
-  override val collectionDetailCardBackgroundPressedColorResource: Int = R.color.collection_detail_background_card_pressed_dark
+  override val collectionDetailCardBackgroundPressedColorResource: Int = collection_detail_background_card_pressed_dark
 
-  override val collectionDetailTextTabSelectedColorResource: Int = R.color.collection_detail_tab_color_selected_dark
+  override val collectionDetailTextTabSelectedColorResource: Int = collection_detail_tab_color_selected_dark
 
-  override val collectionDetailTextTabDefaultColorResource: Int = R.color.collection_detail_tab_color_default_dark
+  override val collectionDetailTextTabDefaultColorResource: Int = collection_detail_tab_color_default_dark
 
 }
 
 case object ThemeLight extends Theme {
 
-  override val searchBackgroundColorResource: Int = R.color.search_background_tint_light
+  override val searchBackgroundColorResource: Int = search_background_tint_light
 
-  override val searchPressedColorResource: Int = R.color.search_press_tint_light
+  override val searchPressedColorResource: Int = search_press_tint_light
 
-  override val searchGoogleColorResource: Int = R.color.search_google_tint_light
+  override val searchGoogleColorResource: Int = search_google_tint_light
 
-  override val searchIconsColorResource: Int = R.color.search_icons_tint_light
+  override val searchIconsColorResource: Int = search_icons_tint_light
 
-  override val appDrawerPressedColorResource: Int = R.color.app_drawer_press_tint_light
+  override val appDrawerPressedColorResource: Int = app_drawer_press_tint_light
 
-  override val collectionDetailBackgroundColorResource: Int = R.color.collection_detail_background_light
+  override val collectionDetailBackgroundColorResource: Int = collection_detail_background_light
 
-  override val collectionDetailTextCardColorResource: Int = R.color.collection_detail_text_card_light
+  override val collectionDetailTextCardColorResource: Int = collection_detail_text_card_light
 
-  override val collectionDetailCardBackgroundColorResource: Int = R.color.collection_detail_background_card_light
+  override val collectionDetailCardBackgroundColorResource: Int = collection_detail_background_card_light
 
-  override val collectionDetailCardBackgroundPressedColorResource: Int = R.color.collection_detail_background_card_pressed_light
+  override val collectionDetailCardBackgroundPressedColorResource: Int = collection_detail_background_card_pressed_light
 
-  override val collectionDetailTextTabSelectedColorResource: Int = R.color.collection_detail_tab_color_selected_light
+  override val collectionDetailTextTabSelectedColorResource: Int = collection_detail_tab_color_selected_light
 
-  override val collectionDetailTextTabDefaultColorResource: Int = R.color.collection_detail_tab_color_default_light
+  override val collectionDetailTextTabDefaultColorResource: Int = collection_detail_tab_color_default_light
 
 }
