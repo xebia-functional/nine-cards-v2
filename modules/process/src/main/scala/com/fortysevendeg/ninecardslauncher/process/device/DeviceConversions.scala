1f7dff6d539ae4b941ef40e35e58f2b5fc86a138
Javi Pacheco <javi@47deg.com>
2015-06-05 09:34:38 +0200

    Bug fixed creating new device

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/DeviceConversions.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/DeviceConversions.scala
@@ -8,6 +8,7 @@ trait DeviceConversions {
   def toAppItemSeq(apps: Seq[Application]) = apps map toAppItem
 
   def toAppItem(app: Application) = {
+    // TODO We need refactor NineCardIntent from Process Layer
 //    val intent = new NineCardIntent(NineCardIntentExtras(
 //      package_name = Some(app.packageName),
 //      class_name = Some(app.className)
@@ -17,7 +18,7 @@ trait DeviceConversions {
       name = app.name,
       packageName = app.packageName,
       imagePath = "image", // TODO create image
-      intent = "")
+      intent = "{\"intentExtras\":{\"class_name\":\"com.moistrue.zombiesmasher.ZombieSmasherActivity\",\"package_name\":\"com.moistrue.zombiesmasher\"},\"action\":\"com.fortysevendeg.ninecardslauncher.OPEN_APP\",\"extras\":{\"classLoader\":{\"parent\":null},\"pairValue\":\"com.moistrue.zombiesmasher\",\"empty\":false,\"parcelled\":false},\"flags\":0.0,\"excludingStopped\":false}")
   }
 
 }
