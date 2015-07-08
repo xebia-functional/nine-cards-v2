ad5d3317cd25e1a7c79ce78058758616575f2102
franciscodr <francisco.d@47deg.com>
2015-07-08 16:31:20 +0200

    9C-168: Adapts unit tests for expecting Task as result

--- modules/repository/src/test/scala/com/fortysevendeg/repository/BaseTestSupport.scala
+++ modules/repository/src/test/scala/com/fortysevendeg/repository/BaseTestSupport.scala
@@ -5,10 +5,12 @@ import org.specs2.specification.{Scope, Around}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Await, Future}
+import scalaz.concurrent.Task
 
 trait BaseTestSupport extends Around with Scope {
 
   def await[T](f: => Future[T]) = Await.result(f, Duration.Inf)
+  def runTask[T](t: => Task[T]) = t.run
   implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
   override def around[T: AsResult](t: => T): Result = AsResult.effectively(t)
 
