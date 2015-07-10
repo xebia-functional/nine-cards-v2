9e5691aac358f283d9b448db4a57850d43a62c18
franciscodr <francisco.d@47deg.com>
2015-07-10 10:14:49 +0200

    9C-170: Removes unused code in BaseTestSupport trait

--- modules/repository/src/test/scala/com/fortysevendeg/repository/BaseTestSupport.scala
+++ modules/repository/src/test/scala/com/fortysevendeg/repository/BaseTestSupport.scala
@@ -1,18 +1,8 @@
 package com.fortysevendeg.repository
 
-import org.specs2.execute.{Result, AsResult}
-import org.specs2.specification.{Scope, Around}
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ExecutionContext, Await, Future}
 import scalaz.concurrent.Task
 
-trait BaseTestSupport extends Around with Scope {
+trait BaseTestSupport {
 
-  def await[T](f: => Future[T]) = Await.result(f, Duration.Inf)
   def runTask[T](t: => Task[T]) = t.run
-  implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
-  override def around[T: AsResult](t: => T): Result = AsResult.effectively(t)
-
-
 }
