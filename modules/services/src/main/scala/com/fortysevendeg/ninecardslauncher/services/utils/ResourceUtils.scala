046c17f785e6a80ee0390bb82bccdf1109f0ea3c
Javi Pacheco <javi@47deg.com>
2015-07-13 11:33:11 +0200

    Interpolation in ResourceUtils

--- modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/utils/ResourceUtils.scala
+++ modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/utils/ResourceUtils.scala
@@ -5,12 +5,11 @@ import com.fortysevendeg.ninecardslauncher.commons.contexts.ContextSupport
 class ResourceUtils {
 
   def getPath(filename: String)(implicit context: ContextSupport): String =
-    String.format("%s/%s", context.getAppIconsDir.getPath, filename)
+    s"${context.getAppIconsDir.getPath}/$filename"
 
   def getPathPackage(packageName: String, className: String)(implicit context: ContextSupport): String =
-    getPath(String.format("%s_%s", packageName.toLowerCase.replace(".", "_"), className.toLowerCase.replace(".", "_")))
+    getPath(s"${replace(packageName)}_${replace(className)}")
 
-  def packageToFilename(packageName: String, className: String): String =
-    String.format("%s_%s", packageName.toLowerCase.replace(".", "_"), className.toLowerCase.replace(".", "_"))
+  private[this] def replace(text: String) = text.toLowerCase.replace(".", "_")
 
 }
