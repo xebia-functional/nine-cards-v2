24f29a7bcc886e516919f6f5608abfee015b7593
Javi Pacheco <javi@47deg.com>
2015-07-21 12:52:19 +0200

    Test for UserConfigProcess

---
+++ modules/process/src/test/scala/com/fortysevendeg/ninecardslauncher/process/userconfig/UserConfigProcessData.scala
@@ -0,0 +1,111 @@
+package com.fortysevendeg.ninecardslauncher.process.userconfig
+
+import com.fortysevendeg.ninecardslauncher.services.api.models._
+import com.fortysevendeg.ninecardslauncher.services.api.RequestConfig
+import play.api.libs.json.{JsString}
+
+import scala.collection.immutable.IndexedSeq
+
+trait UserConfigProcessData {
+
+  val requestConfig = RequestConfig("fake-device-id", "fake-token")
+
+  val email = "example@47deg.com"
+
+  val name = "George"
+
+  val imageUrl = "http://www.47deg.com/image.jpg"
+
+  val deviceIdPrefix = "fake-device-id"
+
+  val firstDeviceId = "fake-device-id-0"
+
+  val noDeviceId = "dont-exits-device-id"
+
+  val deviceName = "Nexus"
+
+  val collectionName = "Example Collection"
+
+  val collectionIcon = "GAME"
+
+  val collectionType = "APPS"
+
+  val collectionCategory = "GAMES"
+
+  val itemType = "APP"
+
+  def createUserConfigCollectionItem(count: Int = 8): Seq[UserConfigCollectionItem] =
+    (0 until count) map {
+      item =>
+        UserConfigCollectionItem(
+          itemType = itemType,
+          title = s"Item $item",
+          metadata = JsString(""),
+          categories = Option(Seq.empty)
+        )
+    }
+
+  def createUserConfigCollection(count: Int = 5) =
+    (0 until count) map {
+      item =>
+        UserConfigCollection(
+          name = collectionName,
+          originalSharedCollectionId = None,
+          sharedCollectionId = None,
+          sharedCollectionSubscribed = Option(true),
+          items = createUserConfigCollectionItem(),
+          collectionType = collectionType,
+          constrains = Seq.empty,
+          wifi = Seq.empty,
+          occurrence = Seq.empty,
+          icon = collectionIcon,
+          radius = 0,
+          lat = 0,
+          lng = 0,
+          alt = 0,
+          category = Option(collectionCategory)
+        )
+    }
+
+  def createUserConfigDevice(count: Int = 3) =
+    (0 until count) map {
+      item =>
+        UserConfigDevice(
+          deviceId = s"$deviceIdPrefix-$item",
+          deviceName = s"$deviceName $item",
+          collections = createUserConfigCollection()
+        )
+    }
+
+  val userConfig = UserConfig(
+    _id = "fake-id",
+    email = email,
+    plusProfile = UserConfigPlusProfile(
+      displayName = name,
+      profileImage = UserConfigProfileImage(
+        imageType = 0,
+        imageUrl = imageUrl,
+        secureUrl = ""
+      )
+    ),
+    devices = createUserConfigDevice(),
+    geoInfo = UserConfigGeoInfo(
+      homeMorning = None,
+      homeNight = None,
+      work = None,
+      current = None
+    ),
+    status = UserConfigStatusInfo(
+      products = Seq.empty,
+      friendsReferred = 0,
+      themesShared = 0,
+      collectionsShared = 0,
+      customCollections = 0,
+      earlyAdopter = false,
+      communityMember = true,
+      joinedThrough = None,
+      tester = false
+    )
+  )
+
+}
