1d098362a775bd37d11e44de535821d0c5b0cff8
Javi Pacheco <javi@47deg.com>
2015-08-05 11:31:09 +0200

    Optimize imports

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/theme/impl/ThemeProcessImpl.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/theme/impl/ThemeProcessImpl.scala
@@ -2,18 +2,13 @@ package com.fortysevendeg.ninecardslauncher.process.theme.impl
 
 import com.fortysevendeg.ninecardslauncher.commons.NineCardExtensions._
 import com.fortysevendeg.ninecardslauncher.commons.contexts.ContextSupport
-import com.fortysevendeg.ninecardslauncher.commons.exceptions.Exceptions.NineCardsException
 import com.fortysevendeg.ninecardslauncher.commons.services.Service
-import com.fortysevendeg.ninecardslauncher.commons.services.Service._
-import com.fortysevendeg.ninecardslauncher.process.theme.{ImplicitsThemeException, ThemeException, ThemeProcess}
 import com.fortysevendeg.ninecardslauncher.process.theme.models.NineCardsTheme
 import com.fortysevendeg.ninecardslauncher.process.theme.models.NineCardsThemeImplicits._
+import com.fortysevendeg.ninecardslauncher.process.theme.{ImplicitsThemeException, ThemeException, ThemeProcess}
 import com.fortysevendeg.ninecardslauncher.process.utils.FileUtils
 import play.api.libs.json.Json
 
-import scalaz.EitherT._
-import scalaz.Scalaz._
-import scalaz.\/
 import scalaz.concurrent.Task
 
 class ThemeProcessImpl
