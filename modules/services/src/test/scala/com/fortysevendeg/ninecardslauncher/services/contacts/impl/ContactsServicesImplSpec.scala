942fba85a7b2297dd6d2a4f679dffc066c3f2159
Fede Fernández <federico.f.b@47deg.com>
2015-08-13 19:12:37 +0200

    Removes invalid tests

--- modules/services/src/test/scala/com/fortysevendeg/ninecardslauncher/services/contacts/impl/ContactsServicesImplSpec.scala
+++ modules/services/src/test/scala/com/fortysevendeg/ninecardslauncher/services/contacts/impl/ContactsServicesImplSpec.scala
@@ -1,8 +1,6 @@
 package com.fortysevendeg.ninecardslauncher.services.contacts.impl
 
 import com.fortysevendeg.ninecardslauncher.commons.contentresolver.ContentResolverWrapperImpl
-import com.fortysevendeg.ninecardslauncher.repository.commons.GeoInfoUri
-import com.fortysevendeg.ninecardslauncher.repository.provider.GeoInfoEntity._
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
@@ -20,25 +18,15 @@ trait ContactsServicesSpecification
 
   trait ValidContactsServicesResponses
     extends ContactsServicesScope
-    with ContactsServicesImplData {
-
-    self: ContactsServicesScope =>
-
-    contentResolverWrapper.fetchAll(
-      uri = GeoInfoUri,
-      projection = allFields)(
-        f = getListFromCursor(geoInfoEntityFromCursor)) returns Seq.empty
-  }
+    with ContactsServicesImplData
 
   trait ErrorContactsServicesResponses
     extends ContactsServicesScope
-    with ContactsServicesImplData {
+    with ContactsServicesImplData
 
-    self: ContactsServicesScope =>
-  }
 }
 
 class ContactsServicesImplSpec
-extends ContactsServicesSpecification {
+  extends ContactsServicesSpecification {
 
 }
