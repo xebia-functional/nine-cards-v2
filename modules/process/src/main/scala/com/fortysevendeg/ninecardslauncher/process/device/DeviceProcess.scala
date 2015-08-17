f22f4c00cfa520881072481bb22f42a3cb1bb2cf
Javi Pacheco <javi@47deg.com>
2015-08-17 16:36:43 +0200

    ScalaDocs added to public methods

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/DeviceProcess.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/DeviceProcess.scala
@@ -8,5 +8,12 @@ trait DeviceProcess {
   def getCategorizedApps(implicit context: ContextSupport): ServiceDef2[Seq[AppCategorized], AppCategorizationException]
   def categorizeApps(implicit context: ContextSupport):  ServiceDef2[Unit, AppCategorizationException]
   def createBitmapsFromPackages(packages: Seq[String])(implicit context: ContextSupport): ServiceDef2[Unit, CreateBitmapException]
+
+  /**
+   * Get the available applications that contain shortcuts creating Intents and Drawables necessaries for UI actions
+   * @return the Seq[com.fortysevendeg.ninecardslauncher.process.device.models.ShortCut] contains
+   *         information about shortcut with the Intents and Drawables for UI actions
+   * @throws ShortCutException if exist some problem to get the shortcuts in the cell phone
+   */
   def getAvailableShortCuts(implicit context: ContextSupport): ServiceDef2[Seq[ShortCut], ShortCutException]
 }
--- modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/shortcuts/ShortCutsServices.scala
+++ modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/shortcuts/ShortCutsServices.scala
@@ -5,5 +5,12 @@ import com.fortysevendeg.ninecardslauncher.commons.services.Service._
 import com.fortysevendeg.ninecardslauncher.services.shortcuts.models.ShortCut
 
 trait ShortCutsServices {
+
+  /**
+   * Get the applications that contains shortcuts to perform specific functions within an app
+   * @return the Seq[com.fortysevendeg.ninecardslauncher.services.shortcuts.models.ShortCut] contains
+   *         information about shortcut for install it, get the icon, etc
+   * @throws ShortCutServicesException if exist some problem to get the shortcuts in the cell phone
+   */
   def getShortCuts(implicit context: ContextSupport): ServiceDef2[Seq[ShortCut], ShortCutServicesException]
 }
