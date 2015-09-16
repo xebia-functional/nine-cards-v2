2baaffd761d6a00a8ace6bfd84e7a0023f218fba
Javi Pacheco <javi@47deg.com>
2015-09-16 09:59:50 +0200

    Fixed weird jumps when you scrolling down in list of apps and contacts and fade when the icon appears

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/commons/NineCardsTweaks.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/commons/NineCardsTweaks.scala
@@ -7,11 +7,15 @@ import android.provider.ContactsContract.Contacts
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.OnScrollListener
 import android.widget.ImageView
+import com.fortysevendeg.macroid.extras.ImageViewTweaks._
+import com.fortysevendeg.macroid.extras.ResourcesExtras._
 import com.bumptech.glide.{DrawableTypeRequest, Glide}
 import com.bumptech.glide.request.animation.GlideAnimation
 import com.bumptech.glide.request.target.SimpleTarget
 import com.fortysevendeg.ninecardslauncher.app.ui.components.CharDrawable
+import com.fortysevendeg.ninecardslauncher2.R
 import macroid.{ActivityContextWrapper, ContextWrapper, Tweak}
+import macroid.FullDsl._
 
 object RecyclerViewListenerTweaks {
   type W = RecyclerView
@@ -74,17 +78,20 @@ object AsyncImageTweaks {
     request: DrawableTypeRequest[_],
     imageView: ImageView,
     char: String,
-    circular: Boolean = false)(implicit context: ActivityContextWrapper) =
-      request
-        .asBitmap()
-        .into(new SimpleTarget[Bitmap]() {
-        override def onLoadFailed(e: Exception, errorDrawable: Drawable): Unit = {
-          imageView.setImageDrawable(new CharDrawable(char, circle = circular))
-        }
+    circular: Boolean = false)(implicit context: ActivityContextWrapper) = {
+    val fadeDuration = resGetInteger(R.integer.fade_duration_new_image)
+    request
+      .asBitmap()
+      .into(new SimpleTarget[Bitmap]() {
+        override def onLoadStarted(placeholder: Drawable): Unit =
+          imageView.setImageDrawable(null)
 
-        override def onResourceReady(resource: Bitmap, glideAnimation: GlideAnimation[_ >: Bitmap]): Unit = {
-          imageView.setImageBitmap(resource)
-        }
+        override def onLoadFailed(e: Exception, errorDrawable: Drawable): Unit =
+          runUi(imageView <~ ivSrc(new CharDrawable(char, circle = circular)) <~ fadeIn(fadeDuration))
+
+        override def onResourceReady(resource: Bitmap, glideAnimation: GlideAnimation[_ >: Bitmap]): Unit =
+          runUi(imageView <~ ivSrc(resource) <~ fadeIn(fadeDuration))
       })
+  }
 
 }
