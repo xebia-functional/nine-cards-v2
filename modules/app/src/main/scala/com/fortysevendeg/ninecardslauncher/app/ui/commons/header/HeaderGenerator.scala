d03c5ad461d36530da92b41f705989f27a4befc6
Fede Fernández <federico.f.b@47deg.com>
2015-11-16 09:29:39 +0100

    9C-280 - Improves the app headers when show the apps ordered by date

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/commons/header/HeaderGenerator.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/commons/header/HeaderGenerator.scala
@@ -21,6 +21,8 @@ trait HeaderGenerator extends HeaderUtils {
 
   val headerUpdated4 = R.string.apps_header_updated_4
 
+  val headerUpdated4Alt = R.string.apps_date
+
   val dayDiffMillis = 1000 * 60 * 60 * 24
 
   val weekDiffMillis = dayDiffMillis * 7
@@ -28,48 +30,51 @@ trait HeaderGenerator extends HeaderUtils {
   val monthDiffMillis = dayDiffMillis * 30
 
   def generateHeaderList(apps: Seq[App], getAppOrder: GetAppOrder)(implicit contextWrapper: ContextWrapper): Seq[AppHeadered] =
-    generateHeaderList(apps = apps, getAppOrder = getAppOrder, actualSeq = Seq.empty, lastHeader = None)
+    generateHeaderList(apps = apps, getAppOrder = getAppOrder, actualSeq = Seq.empty, previousHeaders = Seq.empty)
 
   private[this] def generateHeaderList(
       apps: Seq[App],
       getAppOrder: GetAppOrder,
       actualSeq: Seq[AppHeadered],
-      lastHeader: Option[String])(implicit contextWrapper: ContextWrapper): Seq[AppHeadered] = apps match {
+      previousHeaders: Seq[String])(implicit contextWrapper: ContextWrapper): Seq[AppHeadered] = apps match {
     case Nil => actualSeq
     case Seq(h, t@_ *) =>
-      val header = generateAppHeader(h, getAppOrder)
-      if (lastHeader.contains(header)) {
+      val header = generateAppHeader(h, getAppOrder, previousHeaders)
+      if (previousHeaders.contains(header)) {
         generateHeaderList(
           apps = t,
           getAppOrder = getAppOrder,
           actualSeq = actualSeq :+ AppHeadered(item = Option(h)),
-          lastHeader = lastHeader)
+          previousHeaders = previousHeaders)
       } else {
         generateHeaderList(
           apps = t,
           getAppOrder = getAppOrder,
           actualSeq = actualSeq :+ AppHeadered(header = Some(header)) :+ AppHeadered(item = Option(h)),
-          lastHeader = Some(header))
+          previousHeaders = previousHeaders :+ header)
       }
 
   }
 
-  private[this] def generateAppHeader(app: App, getAppOrder: GetAppOrder)(implicit cw: ContextWrapper): String =
+  private[this] def generateAppHeader(
+      app: App,
+      getAppOrder: GetAppOrder,
+      previousHeaders: Seq[String])(implicit cw: ContextWrapper): String =
     getAppOrder match {
       case GetByName(_) => getCurrentChar(app.name)
       case GetByCategory(_) =>
-        val appCategory = gamesCategories.find(_ == app.category) getOrElse app.category
-        resGetString(appCategory.toLowerCase) getOrElse appCategory.toLowerCase()
+        val appCategory = if (gamesCategories.contains(app.category)) game else app.category
+        resGetString(appCategory.toLowerCase) getOrElse appCategory.toLowerCase
       case GetByInstallDate(_) =>
         val diff = System.currentTimeMillis() - app.dateInstalled
-        resGetString(findStringForDiff(diff))
+        resGetString(findStringForDiff(diff, previousHeaders.isEmpty))
     }
 
-  private[this] def findStringForDiff(diff: Long): Int = diff match {
+  private[this] def findStringForDiff(diff: Long, firstOne: Boolean): Int = diff match {
     case d if d <= dayDiffMillis => headerUpdated1
     case d if d <= weekDiffMillis => headerUpdated2
     case d if d <= monthDiffMillis => headerUpdated3
-    case _ => headerUpdated4
+    case _ => if (firstOne) headerUpdated4Alt else headerUpdated4
   }
 
   def generateContactsForList(contacts: Seq[Contact]): Seq[ContactHeadered] =
