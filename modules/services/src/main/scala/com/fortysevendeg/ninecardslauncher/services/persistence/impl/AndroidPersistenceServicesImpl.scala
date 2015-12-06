0a4e79e51201180ad2ce56f1dc9052733ecd5fb9
Domingo Valera <domingo.v@47deg.com>
2015-12-06 11:46:56 +0100

    dominv 9c 338 substituting null for javaNull

--- modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/persistence/impl/AndroidPersistenceServicesImpl.scala
+++ modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/persistence/impl/AndroidPersistenceServicesImpl.scala
@@ -3,6 +3,7 @@ package com.fortysevendeg.ninecardslauncher.services.persistence.impl
 import android.net.Uri
 import com.fortysevendeg.ninecardslauncher.commons.contexts.ContextSupport
 import com.fortysevendeg.ninecardslauncher.commons.services.Service
+import com.fortysevendeg.ninecardslauncher.commons.javaNull
 import com.fortysevendeg.ninecardslauncher.services.persistence.AndroidIdNotFoundException
 import rapture.core.Result
 
@@ -17,7 +18,7 @@ trait AndroidPersistenceServicesImpl {
   def getAndroidId(implicit context: ContextSupport) =
     Service {
       Task {
-        val cursor = Option(context.getContentResolver.query(Uri.parse(contentGServices), null, null, Array(androidId), null))
+        val cursor = Option(context.getContentResolver.query(Uri.parse(contentGServices), javaNull, javaNull, Array(androidId), javaNull))
         val result = cursor filter (c => c.moveToFirst && c.getColumnCount >= 2) map (_.getLong(1).toHexString.toUpperCase)
         cursor foreach (_.close())
         result map {
