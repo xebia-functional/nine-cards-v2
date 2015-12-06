082b811a9666214f39e88449590d1cd360f2fd2f
Domingo Valera <domingo.v@47deg.com>
2015-12-06 12:29:01 +0100

    dominv 9c 339 Creating new LastCallsDeviceProcessImpl

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/impl/DeviceProcessImpl.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/impl/DeviceProcessImpl.scala
@@ -1,24 +1,16 @@
 package com.fortysevendeg.ninecardslauncher.process.device.impl
 
 import android.graphics.Bitmap
-import com.fortysevendeg.ninecardslauncher.commons.NineCardExtensions._
 import com.fortysevendeg.ninecardslauncher.commons.contexts.ContextSupport
-import com.fortysevendeg.ninecardslauncher.commons.services.Service
-import com.fortysevendeg.ninecardslauncher.commons.services.Service._
 import com.fortysevendeg.ninecardslauncher.process.device._
-import com.fortysevendeg.ninecardslauncher.process.device.models.{Contact, LastCallsContact}
 import com.fortysevendeg.ninecardslauncher.services.api._
 import com.fortysevendeg.ninecardslauncher.services.apps.AppsServices
 import com.fortysevendeg.ninecardslauncher.services.calls.CallsServices
-import com.fortysevendeg.ninecardslauncher.services.calls.models.Call
-import com.fortysevendeg.ninecardslauncher.services.contacts.{ContactsServiceException, ContactsServices, ImplicitsContactsServiceExceptions}
+import com.fortysevendeg.ninecardslauncher.services.contacts.{ContactsServices, ImplicitsContactsServiceExceptions}
 import com.fortysevendeg.ninecardslauncher.services.image._
 import com.fortysevendeg.ninecardslauncher.services.persistence.{ImplicitsPersistenceServiceExceptions, PersistenceServices}
 import com.fortysevendeg.ninecardslauncher.services.shortcuts.ShortcutsServices
 import com.fortysevendeg.ninecardslauncher.services.widgets.WidgetsServices
-import rapture.core.Answer
-
-import scalaz.concurrent.Task
 
 class DeviceProcessImpl(
   val appsService: AppsServices,
@@ -33,6 +25,7 @@ class DeviceProcessImpl(
   with DeviceProcessDependencies
   with AppsDeviceProcessImpl
   with ContactsDeviceProcessImpl
+  with LastCallsDeviceProcessImpl
   with ShorcutsDeviceProcessImpl
   with WidgetsDeviceProcessImpl
   with ImplicitsDeviceException
@@ -65,42 +58,6 @@ class DeviceProcessImpl(
 
   override def getWidgets(implicit context: ContextSupport) = super.getWidgets
 
-  override def getLastCalls(implicit context: ContextSupport) =
-    (for {
-      lastCalls <- callsServices.getLastCalls
-      simpleGroupCalls <- simpleGroupCalls(lastCalls)
-      combinedContacts <- getCombinedContacts(simpleGroupCalls)
-    } yield fillCombinedContacts(combinedContacts)).resolve[CallException]
-
-  private[this] def simpleGroupCalls(lastCalls: Seq[Call]): ServiceDef2[Seq[LastCallsContact], CallException] = Service {
-    Task {
-      CatchAll[CallException] {
-        (lastCalls groupBy (_.number) map { case (k, v) => toSimpleLastCallsContact(k, v) }).toSeq
-      }
-    }
-  }
-
-  private[this] def getCombinedContacts(items: Seq[LastCallsContact]):
-  ServiceDef2[Seq[(LastCallsContact, Option[Contact])], ContactsServiceException] = Service {
-    val tasks = items map (item => combineContact(item).run)
-    Task.gatherUnordered(tasks) map (list => CatchAll[ContactsServiceException](list.collect { case Answer(combinedContact) => combinedContact }))
-  }
-
-  private[this] def combineContact(lastCallsContact: LastCallsContact):
-  ServiceDef2[(LastCallsContact, Option[Contact]), ContactsServiceException] =
-    for {
-      contact <- contactsServices.fetchContactByPhoneNumber(lastCallsContact.number)
-    } yield (lastCallsContact, contact map toContact)
-
-  private[this] def fillCombinedContacts(combinedContacts: Seq[(LastCallsContact, Option[Contact])]): Seq[LastCallsContact] =
-    (combinedContacts map { combinedContact =>
-      val (lastCallsContact, maybeContact) = combinedContact
-      maybeContact map { contact =>
-        lastCallsContact.copy(
-          lookupKey = Some(contact.lookupKey),
-          photoUri = Some(contact.photoUri)
-        )
-      } getOrElse lastCallsContact
-    }).sortWith(_.lastCallDate > _.lastCallDate)
+  override def getLastCalls(implicit context: ContextSupport) = super.getLastCalls
 
 }
