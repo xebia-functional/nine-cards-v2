69936629b223a5793552e8d756d3517e8727f568
Javi Pacheco <javi@47deg.com>
2015-12-14 15:33:42 +0100

    Move DeviceProcess implementations to concrete traits

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/impl/DeviceProcessImpl.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/device/impl/DeviceProcessImpl.scala
@@ -2,20 +2,13 @@ package com.fortysevendeg.ninecardslauncher.process.device.impl
 
 import android.graphics.Bitmap
 import com.fortysevendeg.ninecardslauncher.commons.contexts.ContextSupport
-import com.fortysevendeg.ninecardslauncher.commons.services.Service
-import com.fortysevendeg.ninecardslauncher.commons.services.Service._
-import com.fortysevendeg.ninecardslauncher.process.commons.types.{Misc, NineCardCategory}
 import com.fortysevendeg.ninecardslauncher.process.device._
-import com.fortysevendeg.ninecardslauncher.process.device.models.{Contact, IterableApps, IterableContacts, LastCallsContact}
-import com.fortysevendeg.ninecardslauncher.process.utils.ApiUtils
 import com.fortysevendeg.ninecardslauncher.services.api._
 import com.fortysevendeg.ninecardslauncher.services.apps.AppsServices
 import com.fortysevendeg.ninecardslauncher.services.calls.CallsServices
 import com.fortysevendeg.ninecardslauncher.services.contacts.{ContactsServices, ImplicitsContactsServiceExceptions}
 import com.fortysevendeg.ninecardslauncher.services.image._
 import com.fortysevendeg.ninecardslauncher.services.persistence.{ImplicitsPersistenceServiceExceptions, PersistenceServices}
-import com.fortysevendeg.ninecardslauncher.services.persistence.models.App
-import com.fortysevendeg.ninecardslauncher.services.persistence.{AddAppRequest, ImplicitsPersistenceServiceExceptions, PersistenceServiceException, PersistenceServices}
 import com.fortysevendeg.ninecardslauncher.services.shortcuts.ShortcutsServices
 import com.fortysevendeg.ninecardslauncher.services.widgets.WidgetsServices
 
@@ -42,32 +35,13 @@ class DeviceProcessImpl(
   with ImplicitsContactsServiceExceptions
   with DeviceConversions {
 
-  override def resetSavedItems() = super.resetSavedItems
+  override def resetSavedItems() = super.resetSavedItems()
 
   override def getSavedApps(orderBy: GetAppOrder)(implicit context: ContextSupport) = super.getSavedApps(orderBy)
 
   override def saveInstalledApps(implicit context: ContextSupport) = super.saveInstalledApps
 
-  override def getIterableApps(orderBy: GetAppOrder)(implicit context: ContextSupport) =
-    (for {
-      iter <- persistenceServices.fetchIterableApps(toFetchAppOrder(orderBy), orderBy.ascending)
-    } yield new IterableApps(iter)).resolve[AppException]
-
-  override def saveInstalledApps(implicit context: ContextSupport) =
-    (for {
-      requestConfig <- apiUtils.getRequestConfig
-      installedApps <- appsService.getInstalledApplications
-      googlePlayPackagesResponse <- apiServices.googlePlayPackages(installedApps map (_.packageName))(requestConfig)
-      appPaths <- createBitmapsFromAppPackage(toAppPackageSeq(installedApps))
-      apps = installedApps map { app =>
-        val path = appPaths.find { path =>
-          path.packageName.equals(app.packageName) && path.className.equals(app.className)
-        } map (_.path)
-        val category = googlePlayPackagesResponse.packages find(_.app.docid == app.packageName) flatMap (_.app.details.appDetails.appCategory.headOption)
-        toAddAppRequest(app, (category map (NineCardCategory(_))).getOrElse(Misc), path.getOrElse(""))
-      }
-      _ <- addApps(apps)
-    } yield ()).resolve[AppException]
+  override def getIterableApps(orderBy: GetAppOrder)(implicit context: ContextSupport) = super.getIterableApps(orderBy)
 
   override def saveApp(packageName: String)(implicit context: ContextSupport) = super.saveApp(packageName)
 
@@ -85,21 +59,10 @@ class DeviceProcessImpl(
 
   override def getContacts(filter: ContactsFilter = AllContacts)(implicit context: ContextSupport) = super.getContacts(filter)
 
+  override def getIterableContacts(filter: ContactsFilter = AllContacts)(implicit context: ContextSupport) = super.getIterableContacts(filter)
+
   override def getContact(lookupKey: String)(implicit context: ContextSupport) = super.getContact(lookupKey)
 
-  override def getIterableContacts(filter: ContactsFilter = AllContacts)(implicit context: ContextSupport) =
-    (for {
-      iter <- filter match {
-        case AllContacts => contactsServices.getIterableContacts
-        case FavoriteContacts => contactsServices.getIterableFavoriteContacts
-        case ContactsWithPhoneNumber => contactsServices.getIterableContactsWithPhone
-      }
-    } yield new IterableContacts(iter)).resolve[ContactException]
-
-  override def getContact(lookupKey: String)(implicit context: ContextSupport) =
-    (for {
-      contact <- contactsServices.findContactByLookupKey(lookupKey)
-    } yield toContact(contact)).resolve[ContactException]
   override def getWidgets(implicit context: ContextSupport) = super.getWidgets
 
   override def getLastCalls(implicit context: ContextSupport) = super.getLastCalls
