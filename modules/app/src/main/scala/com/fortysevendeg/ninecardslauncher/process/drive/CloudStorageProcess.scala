ea5ac17d4830540e240594ebcd50711033ffb4a1
Fede Fernández <federico.f.b@47deg.com>
2016-01-14 12:27:22 +0100

    Issue 192 - Adds the documentation for CloudStorageProcess
    [ci skip]

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/process/drive/CloudStorageProcess.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/process/drive/CloudStorageProcess.scala
@@ -5,10 +5,26 @@ import com.fortysevendeg.ninecardslauncher.process.drive.models.{CloudStorageDev
 
 trait CloudStorageProcess {
 
+  /**
+    * Return a sequence of `CloudStorageResource` filtered by device type
+    * @return sequence of `CloudStorageResource`
+    * @throws CloudStorageProcessException if the service throws an error
+    */
   def getCloudStorageDevices(): ServiceDef2[Seq[CloudStorageResource], CloudStorageProcessException]
 
+  /**
+    * Fetch a `CloudStorageDevice` by his id
+    * @param cloudStorageResourceId identifier of the devices
+    * @return the `CloudStorageDevice`
+    * @throws CloudStorageProcessException if the device not exists or the service throws an error
+    */
   def getCloudStorageDevice(cloudStorageResourceId: String): ServiceDef2[CloudStorageDevice, CloudStorageProcessException]
 
+  /**
+    * Create or update a device in the cloud
+    * @param cloudStorageDevice the device to create or update
+    * @throws CloudStorageProcessException if the services throws an error
+    */
   def createOrUpdateCloudStorageDevice(cloudStorageDevice: CloudStorageDevice): ServiceDef2[Unit, CloudStorageProcessException]
 
 }
