7f35a2db6bfdb3deb79be0ec56c70b9abd25f79b
Fede Fernández <federico.f.b@47deg.com>
2016-01-21 16:47:12 +0100

    Issue 194 - Replaces map for foreach in the init cause statement for exceptions in Wizard

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/wizard/Exceptions.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/wizard/Exceptions.scala
@@ -3,15 +3,15 @@ package com.fortysevendeg.ninecardslauncher.app.ui.wizard
 import scalaz.Scalaz._
 
 case class AuthTokenOperationCancelledException(message: String, cause : Option[Throwable] = None) extends RuntimeException(message) {
-  cause map initCause
+  cause foreach initCause
 }
 
 case class AuthTokenException(message: String, cause : Option[Throwable] = None) extends RuntimeException(message) {
-  cause map initCause
+  cause foreach initCause
 }
 
 case class ServiceConnectionException(message: String, cause : Option[Throwable] = None) extends RuntimeException(message) {
-  cause map initCause
+  cause foreach initCause
 }
 
 trait ImplicitsWizardTasksExceptions {
