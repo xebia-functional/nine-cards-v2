7648de991468f777b26e6da9eb5e7c6111931f95
Domingo Valera <domingo.v@47deg.com>
2016-02-22 10:27:13 +0100

    284 - Adding back button functionality

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/preferences/NineCardsPreferencesActivity.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/preferences/NineCardsPreferencesActivity.scala
@@ -5,6 +5,7 @@ import android.preference.Preference.OnPreferenceClickListener
 import android.preference.{Preference, PreferenceFragment, PreferenceActivity}
 import android.view.MenuItem
 import com.fortysevendeg.ninecardslauncher.app.commons._
+import com.fortysevendeg.ninecardslauncher.app.ui.commons.Remove
 import com.fortysevendeg.ninecardslauncher.app.ui.preferences.fragments._
 import com.fortysevendeg.ninecardslauncher2.R
 
@@ -33,42 +34,34 @@ class NineCardsPreferencesActivity
       super.onOptionsItemSelected(item)
   }
 
+  override def onBackPressed() = {
+    super.onBackPressed()
+    actionBar foreach(_.setTitle(R.string.nineCardsSettingsTitle))
+  }
+
   class MyPreferenceFragment
     extends PreferenceFragment {
 
     override def onCreate(savedInstanceState: Bundle) = {
       super.onCreate(savedInstanceState)
       addPreferencesFromResource(R.xml.preferences_headers)
-      findPreference(DefaultLauncherPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(DefaultLauncherPreferences.name, new DefaultLauncherFragment())
-      })
-      findPreference(ThemesPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(ThemesPreferences.name, new ThemesFragment())
-      })
-      findPreference(AppDrawerPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(AppDrawerPreferences.name, new AppDrawerFragment())
-      })
-      findPreference(SizesPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(SizesPreferences.name, new SizesFragment())
-      })
-      findPreference(AnimationsPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(AnimationsPreferences.name, new AnimationsFragment())
-      })
-      findPreference(NewAppPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(NewAppPreferences.name, new NewAppFragment())
-      })
-      findPreference(AboutPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(AboutPreferences.name, new AboutFragment())
-      })
-      findPreference(HelpPreferences.name).setOnPreferenceClickListener(new OnPreferenceClickListener {
-        override def onPreferenceClick(preference: Preference): Boolean = getFragment(HelpPreferences.name, new HelpFragment())
-      })
+      findPreference(DefaultLauncherPreferences.name).setOnPreferenceClickListener(preferenceClick(DefaultLauncherPreferences.name, new DefaultLauncherFragment()))
+      findPreference(ThemesPreferences.name).setOnPreferenceClickListener(preferenceClick(ThemesPreferences.name, new ThemesFragment()))
+      findPreference(AppDrawerPreferences.name).setOnPreferenceClickListener(preferenceClick(AppDrawerPreferences.name, new AppDrawerFragment()))
+      findPreference(SizesPreferences.name).setOnPreferenceClickListener(preferenceClick(SizesPreferences.name, new SizesFragment()))
+      findPreference(AnimationsPreferences.name).setOnPreferenceClickListener(preferenceClick(AnimationsPreferences.name, new AnimationsFragment()))
+      findPreference(NewAppPreferences.name).setOnPreferenceClickListener(preferenceClick(NewAppPreferences.name, new NewAppFragment()))
+      findPreference(AboutPreferences.name).setOnPreferenceClickListener(preferenceClick(AboutPreferences.name, new AboutFragment()))
+      findPreference(HelpPreferences.name).setOnPreferenceClickListener(preferenceClick(HelpPreferences.name, new HelpFragment()))
     }
 
-    private[this] def getFragment(key: String, fragment: PreferenceFragment): Boolean = {
-      getFragmentManager.beginTransaction().addToBackStack(key).replace(android.R.id.content, fragment).commit()
-      true
+    private[this] def preferenceClick(key: String, fragment: PreferenceFragment) = new OnPreferenceClickListener {
+      override def onPreferenceClick(preference: Preference): Boolean = {
+        getFragmentManager.beginTransaction().addToBackStack(key).replace(android.R.id.content, fragment).commit()
+        true
+      }
     }
+
   }
 
 }
