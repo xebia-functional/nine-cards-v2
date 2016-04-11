9413a1b446744bcef08f20bd32e6e0f92b984c31
Javi Pacheco <javi@47deg.com>
2016-04-11 15:06:39 +0200

    RecommendationsFragment using Presenter

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/collections/actions/recommendations/RecommendationsFragment.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/collections/actions/recommendations/RecommendationsFragment.scala
@@ -3,59 +3,30 @@ package com.fortysevendeg.ninecardslauncher.app.ui.collections.actions.recommend
 import android.os.Bundle
 import android.view.View
 import com.fortysevendeg.ninecardslauncher.app.commons.NineCardIntentConversions
-import com.fortysevendeg.ninecardslauncher.app.ui.collections.{CollectionsDetailsActivity, CollectionsPagerPresenter}
-import com.fortysevendeg.ninecardslauncher.app.ui.commons.SafeUi._
-import com.fortysevendeg.ninecardslauncher.app.ui.commons.TasksOps._
+import com.fortysevendeg.ninecardslauncher.app.ui.collections.CollectionsPagerPresenter
 import com.fortysevendeg.ninecardslauncher.app.ui.commons.actions.BaseActionFragment
-import com.fortysevendeg.ninecardslauncher.process.collection.AddCardRequest
-import com.fortysevendeg.ninecardslauncher.process.commons.types.{NoInstalledAppCardType, NineCardCategory}
-import com.fortysevendeg.ninecardslauncher.process.recommendations.models.RecommendedApp
+import com.fortysevendeg.ninecardslauncher.process.commons.types.NineCardCategory
 import com.fortysevendeg.ninecardslauncher2.R
-import macroid.Ui
-
-import scalaz.concurrent.Task
 
 class RecommendationsFragment(implicit collectionsPagerPresenter: CollectionsPagerPresenter)
   extends BaseActionFragment
-  with RecommendationsComposer
-  with NineCardIntentConversions {
+  with RecommendationsIuActionsImpl
+  with NineCardIntentConversions { self =>
+
+  override val collectionsPresenter: CollectionsPagerPresenter = collectionsPagerPresenter
 
   lazy val nineCardCategory = NineCardCategory(getString(Seq(getArguments), RecommendationsFragment.categoryKey, ""))
 
   lazy val packages = getSeqString(Seq(getArguments), BaseActionFragment.packages, Seq.empty[String])
 
+  lazy val presenter = new RecommendationsPresenter(nineCardCategory, packages, self)
+
   override def getLayoutId: Int = R.layout.list_action_fragment
 
   override def onViewCreated(view: View, savedInstanceState: Bundle): Unit = {
     super.onViewCreated(view, savedInstanceState)
-    initUi.run
-    loadRecommendations()
+    presenter.initialize()
   }
-
-  private[this] def loadRecommendations(): Unit = {
-    val task = if (nineCardCategory.isAppCategory) {
-      di.recommendationsProcess.getRecommendedAppsByCategory(nineCardCategory, packages)
-    } else {
-      di.recommendationsProcess.getRecommendedAppsByPackages(packages, packages)
-    }
-    Task.fork(task.run).resolveAsyncUi(
-      onPreTask = () => showLoading,
-      onResult = (recommendations: Seq[RecommendedApp]) => addRecommendations(recommendations, onInstallNowClick),
-      onException = (_) => showError(R.string.errorLoadingRecommendations, loadRecommendations()))
-  }
-
-  def onInstallNowClick(app: RecommendedApp): Ui[_] =
-    Ui {
-      launchGooglePlay(app.packageName)
-      val card = AddCardRequest(
-        term = app.title,
-        packageName = Option(app.packageName),
-        cardType = NoInstalledAppCardType,
-        intent = toNineCardIntent(app),
-        imagePath = "")
-      collectionsPagerPresenter.addCards(Seq(card))
-    } ~
-      unreveal()
 
 }
 
---
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/collections/actions/recommendations/RecommendationsPresenter.scala
@@ -0,0 +1,63 @@
+package com.fortysevendeg.ninecardslauncher.app.ui.collections.actions.recommendations
+
+import com.fortysevendeg.ninecardslauncher.app.commons.NineCardIntentConversions
+import com.fortysevendeg.ninecardslauncher.app.ui.commons.TasksOps._
+import com.fortysevendeg.ninecardslauncher.app.ui.commons.{LauncherExecutor, Presenter}
+import com.fortysevendeg.ninecardslauncher.process.collection.AddCardRequest
+import com.fortysevendeg.ninecardslauncher.process.commons.types.{NineCardCategory, NoInstalledAppCardType}
+import com.fortysevendeg.ninecardslauncher.process.recommendations.models.RecommendedApp
+import macroid.{ActivityContextWrapper, Ui}
+
+import scalaz.concurrent.Task
+
+class RecommendationsPresenter(
+  category: NineCardCategory,
+  packages: Seq[String],
+  actions: RecommendationsIuActions)(implicit activityContextWrapper: ActivityContextWrapper)
+  extends Presenter
+  with NineCardIntentConversions
+  with LauncherExecutor {
+
+  def initialize(): Unit = {
+    actions.initialize().run
+    loadRecommendations()
+  }
+
+  def installNow(app: RecommendedApp): Unit = {
+    launchGooglePlay(app.packageName)
+    val card = AddCardRequest(
+      term = app.title,
+      packageName = Option(app.packageName),
+      cardType = NoInstalledAppCardType,
+      intent = toNineCardIntent(app),
+      imagePath = "")
+    actions.recommendationAdded(card).run
+  }
+
+  def loadRecommendations(): Unit = {
+    val task = if (category.isAppCategory) {
+      di.recommendationsProcess.getRecommendedAppsByCategory(category, packages)
+    } else {
+      di.recommendationsProcess.getRecommendedAppsByPackages(packages, packages)
+    }
+    Task.fork(task.run).resolveAsyncUi(
+      onPreTask = () => actions.showLoading(),
+      onResult = (recommendations: Seq[RecommendedApp]) => actions.loadRecommendations(recommendations),
+      onException = (_) => actions.showLoadingRecommendationError())
+  }
+
+}
+
+trait RecommendationsIuActions {
+
+  def initialize(): Ui[Any]
+
+  def showLoading(): Ui[Any]
+
+  def showLoadingRecommendationError(): Ui[Any]
+
+  def loadRecommendations(recommendations: Seq[RecommendedApp]): Ui[Any]
+
+  def recommendationAdded(card: AddCardRequest): Ui[Any]
+
+}
