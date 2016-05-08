b5311ab6f49552100c6b9bf7f6471de3257a518c
Javi Pacheco <javi@47deg.com>
2016-05-08 13:29:02 +0200

    Upgrade Scala Plugin

--- project/Settings.scala
+++ project/Settings.scala
@@ -50,9 +50,10 @@ object Settings {
       dexMaxHeap in Android := "2048m",
       proguardScala in Android := true,
       useProguard in Android := true,
+      useProguardInDebug in Android := true,
       versionName in Android := Some(s"${versionName.value.getOrElse("")}-$versionNameSuffix"),
       proguardOptions in Android ++= proguardCommons,
-      proguardCache in Android := Seq.empty)
+      proguardCache in Android := proguardCacheList)
 
   // Api Module
   lazy val apiSettings = basicSettings ++ librarySettings ++
@@ -175,7 +176,6 @@ object Settings {
 
   lazy val multiDex = Seq(
     dexMulti in Android := true,
-    dexMinimizeMain in Android := true,
     dexMainClasses in Android := multiDexClasses
   )
 
