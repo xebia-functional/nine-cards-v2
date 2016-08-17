6397d8d91cc5d5bd44d2f9e099fa938c7c4bdd46
Ana <ana@47deg.com>
2016-08-17 12:12:38 +0200

    Migrated apps with their tests.

--- modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/apps/Exceptions.scala
+++ modules/services/src/main/scala/com/fortysevendeg/ninecardslauncher/services/apps/Exceptions.scala
@@ -1,11 +1,13 @@
 package com.fortysevendeg.ninecardslauncher.services.apps
 
-import scalaz.Scalaz._
+import com.fortysevendeg.ninecardslauncher.commons.services.CatsService.NineCardException
 
-case class AppsInstalledException(message: String, cause : Option[Throwable] = None) extends RuntimeException(message) {
+case class AppsInstalledException(message: String, cause : Option[Throwable] = None)
+  extends RuntimeException(message)
+  with NineCardException {
   cause map initCause
 }
 
 trait ImplicitsAppsExceptions {
-  implicit def appsInstalledExceptionConverter = (t: Throwable) => AppsInstalledException(t.getMessage, t.some)
+  implicit def appsInstalledExceptionConverter = (t: Throwable) => AppsInstalledException(t.getMessage, Option(t))
 }
