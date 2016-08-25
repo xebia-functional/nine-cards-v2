de7d8470c50e182d84d10ef19f4f0ed5df36c332
Javi Pacheco <javi@47deg.com>
2016-08-25 18:30:56 +0200

    New design for icon in top bar for moments

---
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/app/ui/components/drawables/TopBarMomentEdgeBackgroundDrawable.scala
@@ -0,0 +1,46 @@
+package com.fortysevendeg.ninecardslauncher.app.ui.components.drawables
+
+import android.graphics._
+import android.graphics.drawable.Drawable
+import com.fortysevendeg.macroid.extras.ResourcesExtras._
+import com.fortysevendeg.ninecardslauncher.app.ui.commons.ColorOps._
+import com.fortysevendeg.ninecardslauncher.process.theme.models.{NineCardsTheme, SearchBackgroundColor}
+import com.fortysevendeg.ninecardslauncher2.R
+import macroid.ContextWrapper
+
+class TopBarMomentEdgeBackgroundDrawable(implicit theme: NineCardsTheme, contextWrapper: ContextWrapper)
+  extends Drawable {
+
+  val color = theme.get(SearchBackgroundColor).alpha(.2f)
+
+  val radius = resGetDimensionPixelSize(R.dimen.radius_default)
+
+  val height = resGetDimensionPixelSize(R.dimen.height_icon_content_top_bar_moment) - radius
+
+  val width = resGetDimensionPixelSize(R.dimen.width_icon_content_top_bar_moment)
+
+  val size = resGetDimensionPixelSize(R.dimen.padding_large) + (width / 2) - radius
+
+  val paint: Paint = {
+    val paint = new Paint(Paint.ANTI_ALIAS_FLAG)
+    paint.setColor(color)
+    paint
+  }
+
+  override def draw(canvas: Canvas): Unit = {
+    val bounds = getBounds
+    val verticalPadding = (bounds.height() - height) / 2
+    canvas.drawRect(
+      bounds.left,
+      verticalPadding + (radius / 2),
+      size,
+      bounds.height() - verticalPadding - (radius / 2),
+      paint)
+  }
+
+  override def setColorFilter(cf: ColorFilter): Unit = paint.setColorFilter(cf)
+
+  override def setAlpha(alpha: Int): Unit = paint.setAlpha(alpha)
+
+  override def getOpacity: Int = PixelFormat.TRANSPARENT
+}
