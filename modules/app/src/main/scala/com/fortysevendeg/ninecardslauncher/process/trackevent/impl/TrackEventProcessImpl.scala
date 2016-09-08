2ce3a71e70c0b7b279408b90631105dd2f60a108
Javi Pacheco <javi@47deg.com>
2016-09-08 17:30:47 +0200

    Games send 2 events to analytics

--- modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/process/trackevent/impl/TrackEventProcessImpl.scala
+++ modules/app/src/main/scala/com/fortysevendeg/ninecardslauncher/process/trackevent/impl/TrackEventProcessImpl.scala
@@ -1,12 +1,22 @@
 package com.fortysevendeg.ninecardslauncher.process.trackevent.impl
 
+import cats.data.Xor
 import com.fortysevendeg.ninecardslauncher.commons.NineCardExtensions._
+import com.fortysevendeg.ninecardslauncher.commons.services.TaskService._
+import com.fortysevendeg.ninecardslauncher.commons.services.TaskService
+import com.fortysevendeg.ninecardslauncher.process.commons.types.Game
 import com.fortysevendeg.ninecardslauncher.process.trackevent._
 import com.fortysevendeg.ninecardslauncher.services.analytics.{AnalyticEvent, AnalyticsServices}
+import cats.implicits._
+import com.fortysevendeg.ninecardslauncher.commons.services.TaskService.TaskService
+
+import scalaz.concurrent.Task
 
 class TrackEventProcessImpl(analyticsServices: AnalyticsServices)
   extends TrackEventProcess
   with ImplicitsTrackEventException {
+
+  private[this] val startNameGame = "GAME_"
 
   override def openAppFromAppDrawer(packageName: String, category: Category) = {
     val event = AnalyticEvent(
@@ -15,7 +25,14 @@ class TrackEventProcessImpl(analyticsServices: AnalyticsServices)
       action = OpenAction.name,
       label = Option(packageName),
       value = Option(OpenAppFromAppDrawerValue.value))
-    analyticsServices.trackEvent(event).resolve[TrackEventException]
+
+    def eventForGames(isGame: Boolean): TaskService[Unit] = if (isGame) {
+      analyticsServices.trackEvent(event.copy(category = Game.name)).resolve[TrackEventException]
+    } else {
+      TaskService(Task(Xor.Right(())))
+    }
+
+    (analyticsServices.trackEvent(event) *> eventForGames(category.name.startsWith(startNameGame))).resolve[TrackEventException]
   }
 
   override def openAppFromCollection(packageName: String, category: Category) = {
