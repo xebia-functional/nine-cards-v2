0f71f237cfd60a67945d6d80f4dd78246b7b6166
Fede Fernández <federico.f.b@47deg.com>
2016-09-26 12:57:59 +0200

    Uses Task.async for getAuthToken

--- modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/accounts/impl/UserAccountsProcessImpl.scala
+++ modules/process/src/main/scala/com/fortysevendeg/ninecardslauncher/process/accounts/impl/UserAccountsProcessImpl.scala
@@ -1,12 +1,16 @@
 package com.fortysevendeg.ninecardslauncher.process.accounts.impl
 
-import android.accounts.{Account => AndroidAccount, AccountManager, OperationCanceledException}
+import android.accounts.{AccountManager, AccountManagerCallback, AccountManagerFuture, OperationCanceledException}
+import android.os.Bundle
 import cats.syntax.either._
 import com.fortysevendeg.ninecardslauncher.commons._
 import com.fortysevendeg.ninecardslauncher.commons.contexts.{ActivityContextSupport, ContextSupport}
 import com.fortysevendeg.ninecardslauncher.commons.services.TaskService
 import com.fortysevendeg.ninecardslauncher.process.accounts._
 import monix.eval.Task
+import monix.execution.Cancelable
+
+import scala.util.{Failure, Success, Try}
 
 class UserAccountsProcessImpl
   extends UserAccountsProcess
@@ -33,15 +37,27 @@ class UserAccountsProcessImpl
 
   override def getAuthToken(accountName: String, scope: String)(implicit contextSupport: ActivityContextSupport) = {
     TaskService {
-      Task {
-        Either.catchNonFatal {
-          val activity = contextSupport.getActivity.getOrElse(throw new IllegalStateException("Activity instance is null"))
-          val result = contextSupport.getAccountManager.getAuthToken(new AndroidAccount(accountName, googleAccountType), scope, javaNull, activity, javaNull, javaNull).getResult
-          Option(result.getString(AccountManager.KEY_AUTHTOKEN)) getOrElse (throw new RuntimeException("Received null token"))
-        } leftMap {
-          case e: OperationCanceledException => UserAccountsProcessOperationCancelledException(e.getMessage, Some(e))
-          case t: Throwable => UserAccountsProcessExceptionImpl(t.getMessage, Some(t))
-        }
+      Task.async[UserAccountsProcessException Either String] { (scheduler, callback) =>
+        val activity = contextSupport.getActivity.getOrElse(throw new IllegalStateException("Activity instance is null"))
+        val androidAccount = new android.accounts.Account(accountName, googleAccountType)
+        contextSupport.getAccountManager.getAuthToken(androidAccount, scope, javaNull, activity, new AccountManagerCallback[Bundle] {
+          override def run(future: AccountManagerFuture[Bundle]): Unit = {
+            Try {
+              val bundle = future.getResult
+              Option(bundle.getString(AccountManager.KEY_AUTHTOKEN)) getOrElse (throw new RuntimeException("Received null token"))
+            } match {
+              case Success(token) =>
+                callback(Success(Right(token)))
+              case Failure(e: OperationCanceledException) =>
+                callback(Success(Left(UserAccountsProcessOperationCancelledException(e.getMessage, Some(e)))))
+              case Failure(e: SecurityException) =>
+                callback(Success(Left(UserAccountsProcessPermissionException(e.getMessage, Some(e)))))
+              case Failure(e) =>
+                callback(Success(Left(UserAccountsProcessExceptionImpl(e.getMessage, Some(e)))))
+            }
+          }
+        }, javaNull)
+        Cancelable.empty
       }
     }
   }
