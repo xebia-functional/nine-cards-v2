b7a308c6f0ea3e867ba51e0118a6653d8338d3aa
Domingo Valera <domingo.v@47deg.com>
2016-10-06 15:30:32 +0200

    903 - Code review

--- modules/services/src/main/scala/cards/nine/services/track/TrackServices.scala
+++ modules/services/src/main/scala/cards/nine/services/track/TrackServices.scala
@@ -7,7 +7,7 @@ trait TrackServices {
 
   /**
     * Track event in Google Analytics
- *
+    *
     * @throws TrackServicesException if there was an error with the request GoogleAnalytics
     */
   def trackEvent(event: TrackEvent): TaskService[Unit]
