f15aac1bca483f6c40d6534527f156d78ff12494
Domingo Valera <domingo.v@47deg.com>
2016-11-14 03:41:34 +0100

    1093 - Using new track events in jobs

--- modules/app/src/main/scala/cards/nine/app/ui/launcher/jobs/AppDrawerJobs.scala
+++ modules/app/src/main/scala/cards/nine/app/ui/launcher/jobs/AppDrawerJobs.scala
@@ -15,6 +15,7 @@ class AppDrawerJobs(
 
   def loadSearch(query: String): TaskService[Unit] = {
     for {
+      _ <- di.trackEventProcess.goToGooglePlayButton()
       _ <- mainAppDrawerUiActions.showLoadingInGooglePlay()
       result <- di.recommendationsProcess.searchApps(query)
       _ <- mainAppDrawerUiActions.reloadSearchInDrawer(result)
@@ -24,6 +25,7 @@ class AppDrawerJobs(
   def loadApps(appsMenuOption: AppsMenuOption): TaskService[Unit] = {
     val getAppOrder = toGetAppOrder(appsMenuOption)
     for {
+      _ <- di.trackEventProcess.goToApps()
       result <- getLoadApps(getAppOrder)
       (apps, counters) = result
       _ <- mainAppDrawerUiActions.reloadAppsInDrawer(
@@ -50,6 +52,7 @@ class AppDrawerJobs(
       case _ =>
         val getContactFilter = toGetContactFilter(contactsMenuOption)
         for {
+          _ <- di.trackEventProcess.goToApps()
           result <- getLoadContacts(getContactFilter)
           (contacts, counters) = result
           _ <- mainAppDrawerUiActions.reloadContactsInDrawer(contacts, counters)
@@ -77,6 +80,7 @@ class AppDrawerJobs(
 
   private[this] def getLoadApps(order: GetAppOrder): TaskService[(IterableApps, Seq[TermCounter])] =
     for {
+      _ <- di.trackEventProcess.goToFiltersByButton(order.name)
       iterableApps <- di.deviceProcess.getIterableApps(order)
       counters <- di.deviceProcess.getTermCountersForApps(order)
     } yield (iterableApps, counters)
