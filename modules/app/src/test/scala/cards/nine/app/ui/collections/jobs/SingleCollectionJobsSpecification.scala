226495c48c7768daa7d6aea75dd01fb38c1c7513
Ana <ana@47deg.com>
2016-11-19 20:42:42 +0100

    Added lasts test

--- modules/app/src/test/scala/cards/nine/app/ui/collections/jobs/SingleCollectionJobsSpecification.scala
+++ modules/app/src/test/scala/cards/nine/app/ui/collections/jobs/SingleCollectionJobsSpecification.scala
@@ -2,6 +2,7 @@ package cards.nine.app.ui.collections.jobs
 
 import cards.nine.app.di.Injector
 import cards.nine.app.ui.collections.jobs.uiactions.{ScrollUp, SingleCollectionUiActions}
+import cards.nine.app.ui.commons.JobException
 import cards.nine.app.ui.launcher.jobs.LauncherTestData
 import cards.nine.commons.test.TaskServiceSpecification
 import cards.nine.commons.test.data.CollectionTestData
@@ -11,7 +12,8 @@ import cards.nine.process.trackevent.TrackEventProcess
 import macroid.ActivityContextWrapper
 import org.specs2.mock.Mockito
 import org.specs2.specification.Scope
-
+import cards.nine.commons.test.data.CollectionValues._
+import cards.nine.commons.test.data.CardValues._
 
 trait SingleCollectionJobsSpecification
   extends TaskServiceSpecification
@@ -131,19 +133,228 @@ class SingleCollectionJobsSpec
   }
 
   "addCards" should {
-    "return a valid response when the service returns a right response" in new SingleCollectionJobsScope {
+    "returns a valid response when the service returns a right response" in new SingleCollectionJobsScope {
 
       mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection))
-      mockTrackEventProcess.addAppToCollection(any,any) returns serviceRight(Unit)
+      mockTrackEventProcess.addAppToCollection(any, any) returns serviceRight(Unit)
       mockSingleCollectionUiActions.addCards(any) returns serviceRight(Unit)
 
       singleCollectionJobs.addCards(seqCard).mustRightUnit
 
       there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
-      there was exactly(seqCard.size)(mockTrackEventProcess).addAppToCollection(any,any)
+      there was exactly(seqCard.size)(mockTrackEventProcess).addAppToCollection(any, any)
       there was one(mockSingleCollectionUiActions).addCards(seqCard)
     }
 
+    "returns a valid response but not track nothing when don't have current collection" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(None)
+      mockSingleCollectionUiActions.addCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.addCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).addCards(seqCard)
+    }
+
+    "returns a valid response and track although don't have apps Category, create a moment Category" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection.copy(appsCategory = None)))
+      mockTrackEventProcess.addAppToCollection(any, any) returns serviceRight(Unit)
+      mockSingleCollectionUiActions.addCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.addCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).addCards(seqCard)
+    }
+
+    "returns a valid response but not track nothing when don't have appsCategory and moment" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection.copy(appsCategory = None, moment = None)))
+      mockSingleCollectionUiActions.addCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.addCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).addCards(seqCard)
+    }
+
+    "Does nothing if cards is Seq.empty" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.addCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.addCards(Seq.empty).mustRightUnit
+
+      there was no(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).addCards(Seq.empty)
+    }
+
+  }
+  "removeCards" should {
+    "returns a valid response when the service returns a right response" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection))
+      mockTrackEventProcess.removeFromCollection(any, any) returns serviceRight(Unit)
+      mockSingleCollectionUiActions.removeCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.removeCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was exactly(seqCard.size)(mockTrackEventProcess).removeFromCollection(any, any)
+      there was one(mockSingleCollectionUiActions).removeCards(seqCard)
+    }
+
+    "returns a valid response but not track nothing when don't have current collection" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(None)
+      mockSingleCollectionUiActions.removeCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.removeCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).removeCards(seqCard)
+    }
+
+    "returns a valid response and track although don't have apps Category, create a moment Category" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection.copy(appsCategory = None)))
+      mockTrackEventProcess.removeFromCollection(any, any) returns serviceRight(Unit)
+      mockSingleCollectionUiActions.removeCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.removeCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).removeCards(seqCard)
+    }
+
+    "returns a valid response but not track nothing when don't have appsCategory and moment" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.getCurrentCollection returns serviceRight(Option(collection.copy(appsCategory = None, moment = None)))
+      mockSingleCollectionUiActions.removeCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.removeCards(seqCard).mustRightUnit
+
+      there was exactly(seqCard.size)(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).removeCards(seqCard)
+    }
+
+    "Does nothing if cards is Seq.empty" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.removeCards(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.removeCards(Seq.empty).mustRightUnit
+
+      there was no(mockSingleCollectionUiActions).getCurrentCollection
+      there was one(mockSingleCollectionUiActions).removeCards(Seq.empty)
+    }
+
+  }
+
+  "reloadCards" should {
+    "calls to action reloadCards" in new SingleCollectionJobsScope {
+      mockSingleCollectionUiActions.reloadCards(any) returns serviceRight(Unit)
+      singleCollectionJobs.reloadCards(seqCard).mustRightUnit
+      there was one(mockSingleCollectionUiActions).reloadCards(seqCard)
+    }
+  }
+
+  "bindAnimatedAdapter" should {
+    "calls to bindAnimatedAdapter" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.bindAnimatedAdapter(any,any) returns serviceRight(Unit)
+      singleCollectionJobs.bindAnimatedAdapter().mustRightUnit
+      there was one(mockSingleCollectionUiActions).bindAnimatedAdapter(true,collection)
+    }
+
+    "return a JobException if the service throws an exception" in new SingleCollectionJobsScope {
+
+      override val singleCollectionJobs = new SingleCollectionJobs(mockAnimateCards, None, mockSingleCollectionUiActions)(contextWrapper) {
+
+        override lazy val di: Injector = mockInjector
+
+      }
+      singleCollectionJobs.bindAnimatedAdapter().mustLeft[JobException]
+      there was no(mockSingleCollectionUiActions).bindAnimatedAdapter(any,any)
+    }
+  }
+
+  "saveEditedCard" should {
+    "Save the edited card" in new SingleCollectionJobsScope {
+
+      mockCollectionProcess.editCard(any,any,any) returns serviceRight(card)
+      mockSingleCollectionUiActions.reloadCard(any) returns serviceRight(Unit)
+
+      singleCollectionJobs.saveEditedCard(collection.id, card.id,Option(newCardName)).mustRightUnit
+
+      there was one(mockCollectionProcess).editCard(collection.id,card.id,newCardName)
+      there was one(mockSingleCollectionUiActions).reloadCard(card)
+    }
+
+    "Shows a message form field error if cardName is an empty string" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.showMessageFormFieldError returns serviceRight(Unit)
+
+      singleCollectionJobs.saveEditedCard(collection.id, card.id,Option("")).mustRightUnit
+
+      there was one(mockSingleCollectionUiActions).showMessageFormFieldError
+    }
+
+    "Shows a message form field error if don't have cardName" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.showMessageFormFieldError returns serviceRight(Unit)
+
+      singleCollectionJobs.saveEditedCard(collection.id, card.id,None).mustRightUnit
+
+      there was one(mockSingleCollectionUiActions).showMessageFormFieldError
+    }
+  }
+
+  "showData" should {
+    "calls to show data" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.showData(any) returns serviceRight(Unit)
+      singleCollectionJobs.showData().mustRightUnit
+      there was one(mockSingleCollectionUiActions).showData(collection.cards.isEmpty)
+    }
+
+    "return a JobException if the service throws an exception" in new SingleCollectionJobsScope {
+
+      override val singleCollectionJobs = new SingleCollectionJobs(mockAnimateCards, None, mockSingleCollectionUiActions)(contextWrapper) {
+
+        override lazy val di: Injector = mockInjector
+
+      }
+      singleCollectionJobs.showData().mustLeft[JobException]
+      there was no(mockSingleCollectionUiActions).showData(any)
+    }
+  }
+
+  "updateScroll" should {
+    "calls to action updateScroll" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.updateVerticalScroll(any) returns serviceRight(Unit)
+      singleCollectionJobs.updateScroll(scrollY).mustRightUnit
+      there was one(mockSingleCollectionUiActions).updateVerticalScroll(scrollY)
+    }
+  }
+
+  "setScrollType" should {
+    "calls to action setScrollType" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.scrollType(any) returns serviceRight(Unit)
+      singleCollectionJobs.setScrollType(ScrollUp).mustRightUnit
+      there was one(mockSingleCollectionUiActions).scrollType(ScrollUp)
+    }
+  }
+
+  "showGenericError" should {
+    "calls to action showGenericError" in new SingleCollectionJobsScope {
+
+      mockSingleCollectionUiActions.showContactUsError() returns serviceRight(Unit)
+      singleCollectionJobs.showGenericError().mustRightUnit
+      there was one(mockSingleCollectionUiActions).showContactUsError()
+    }
   }
 
 }
