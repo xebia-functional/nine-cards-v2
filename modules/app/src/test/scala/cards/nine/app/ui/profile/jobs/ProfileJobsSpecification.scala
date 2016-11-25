f0ee9d4b093fe8bea23292ceea4d993ecb19e0bc
Ana <ana@47deg.com>
2016-11-25 13:38:50 +0100

    Added more tests

--- modules/app/src/test/scala/cards/nine/app/ui/profile/jobs/ProfileJobsSpecification.scala
+++ modules/app/src/test/scala/cards/nine/app/ui/profile/jobs/ProfileJobsSpecification.scala
@@ -1,14 +1,19 @@
 package cards.nine.app.ui.profile.jobs
 
 import cards.nine.app.di.Injector
-import cards.nine.app.ui.commons.JobException
+import cards.nine.app.ui.commons.{BroadAction, JobException}
 import cards.nine.app.ui.launcher.jobs.LauncherTestData
 import cards.nine.commons.services.TaskService
 import cards.nine.commons.test.TaskServiceSpecification
-import cards.nine.commons.test.data.UserTestData
+import cards.nine.commons.test.data.{SharedCollectionTestData, UserTestData}
 import cards.nine.process.cloud.CloudStorageProcess
+import cards.nine.process.collection.{CollectionException, CollectionProcess}
+import cards.nine.process.device.{AppException, DeviceProcess}
+import cards.nine.process.intents.LauncherExecutorProcess
+import cards.nine.process.sharedcollections.SharedCollectionsProcess
 import cards.nine.process.theme.ThemeProcess
-import cards.nine.process.user.UserProcess
+import cards.nine.process.trackevent.TrackEventProcess
+import cards.nine.process.user.{UserException, UserProcess}
 import com.google.android.gms.common.api.GoogleApiClient
 import macroid.ActivityContextWrapper
 import org.specs2.mock.Mockito
@@ -21,8 +26,9 @@ trait ProfileJobsSpecification extends TaskServiceSpecification
 
   trait ProfileJobsScope
     extends Scope
-    with LauncherTestData
-    with UserTestData {
+      with LauncherTestData
+      with UserTestData
+      with SharedCollectionTestData {
 
     implicit val contextWrapper = mock[ActivityContextWrapper]
 
@@ -44,6 +50,26 @@ trait ProfileJobsSpecification extends TaskServiceSpecification
 
     mockInjector.cloudStorageProcess returns mockCloudStorageProcess
 
+    val mockTrackEventProcess = mock[TrackEventProcess]
+
+    mockInjector.trackEventProcess returns mockTrackEventProcess
+
+    val mockDeviceProcess = mock[DeviceProcess]
+
+    mockInjector.deviceProcess returns mockDeviceProcess
+
+    val mockCollectionProcess = mock[CollectionProcess]
+
+    mockInjector.collectionProcess returns mockCollectionProcess
+
+    val mockLauncherExecutorProcess = mock[LauncherExecutorProcess]
+
+    mockInjector.launcherExecutorProcess returns mockLauncherExecutorProcess
+
+    val mockSharedCollectionsProcess = mock[SharedCollectionsProcess]
+
+    mockInjector.sharedCollectionsProcess returns mockSharedCollectionsProcess
+
     val profileJobs = new ProfileJobs(mockProfileUiActions)(contextWrapper) {
 
       override lazy val di: Injector = mockInjector
@@ -51,6 +77,13 @@ trait ProfileJobsSpecification extends TaskServiceSpecification
       override def getThemeTask = TaskService.right(theme)
 
       override def themeFile = ""
+
+      override def sendBroadCastTask(broadAction: BroadAction) = TaskService.empty
+
+      override def getString(res: Int): String = ""
+
+      override def getString(res: Int, args: AnyRef*): String = ""
+
     }
 
   }
@@ -113,12 +146,242 @@ class ProfileJobsSpec
       mockProfileUiActions.initialize(any) returns serviceRight(Unit)
       mockUserProcess.getUser(any) returns serviceRight(user)
       mockCloudStorageProcess.createCloudStorageClient(any)(any) returns serviceRight(mockApiClient)
-      mockProfileUiActions.userProfile(any,any,any) returns serviceRight(Unit)
+      mockProfileUiActions.userProfile(any, any, any) returns serviceRight(Unit)
 
       profileJobs.initialize().mustRightUnit
 
+      there was one(mockProfileUiActions).initialize(theme)
+      there was one(mockCloudStorageProcess).createCloudStorageClient(===(user.email.getOrElse("")))(any)
+      there was one(mockProfileUiActions).userProfile(user.userProfile.name, user.email.getOrElse(""), user.userProfile.avatar)
     }
 
+    "returns a JobException when the user doesn't have email" in new ProfileJobsScope {
+
+      mockThemeProcess.getTheme(any)(any) returns serviceRight(theme)
+      mockProfileUiActions.initialize(any) returns serviceRight(Unit)
+      mockUserProcess.getUser(any) returns serviceRight(user.copy(email = None))
+
+      profileJobs.initialize().mustLeft[JobException]
+
+      there was one(mockProfileUiActions).initialize(theme)
+    }
+
+    "returns a JobException when the process returns a exception" in new ProfileJobsScope {
+
+      mockThemeProcess.getTheme(any)(any) returns serviceRight(theme)
+      mockProfileUiActions.initialize(any) returns serviceRight(Unit)
+      mockUserProcess.getUser(any) returns serviceLeft(UserException(""))
+
+      profileJobs.initialize().mustLeft[UserException]
+
+      there was one(mockProfileUiActions).initialize(theme)
+    }
   }
 
+  "resume" should {
+    "call to askBroadCastTask and return a valid response when the service returns a right response" in new ProfileJobsScope {
+
+      override val profileJobs = new ProfileJobs(mockProfileUiActions)(contextWrapper) {
+        override def askBroadCastTask(broadAction: BroadAction) = TaskService.empty
+      }
+
+      profileJobs.resume().mustRightUnit
+    }
+
+    "returns a JobException when call to askBroadCastTask and return an exception" in new ProfileJobsScope {
+
+      override val profileJobs = new ProfileJobs(mockProfileUiActions)(contextWrapper) {
+        override def sendBroadCastTask(broadAction: BroadAction) = TaskService.left(JobException(""))
+      }
+
+      profileJobs.resume().mustLeft[JobException]
+    }
+  }
+
+  "stop" should {
+    "returns a valid response when apiclient is disconnect" in new ProfileJobsScope {
+
+      statuses = statuses.copy(apiClient = Option(mockApiClient))
+      profileJobs.stop().mustRightUnit
+    }
+
+    "Do nothing when ApiClient is None" in new ProfileJobsScope {
+
+      statuses = statuses.copy(apiClient = None)
+      profileJobs.stop().mustRightUnit
+    }
+
+    "returns a JobException when the disconnect throws a exception" in new ProfileJobsScope {
+
+      statuses = statuses.copy(apiClient = Option(mockApiClient))
+      mockApiClient.disconnect() throws new RuntimeException("")
+      profileJobs.stop().mustLeft[JobException]
+    }
+  }
+
+  "onOffsetChanged" should {
+    "returns a valid response when the actions return a right responses" in new ProfileJobsScope {
+      val percentage: Float = 1
+
+      mockProfileUiActions.handleToolbarVisibility(any) returns serviceRight(Unit)
+      mockProfileUiActions.handleProfileVisibility(any) returns serviceRight(Unit)
+
+      profileJobs.onOffsetChanged(percentage).mustRightUnit
+
+      there was one(mockProfileUiActions).handleToolbarVisibility(percentage)
+      there was one(mockProfileUiActions).handleProfileVisibility(percentage)
+    }
+  }
+
+  "errorSyncing" should {
+    "shows a message syncing error" in new ProfileJobsScope {
+
+      mockProfileUiActions.showSyncingError() returns serviceRight(Unit)
+
+      profileJobs.errorSyncing().mustRightUnit
+
+      there was one(mockProfileUiActions).showSyncingError()
+      profileJobs.syncEnabled shouldEqual true
+    }
+  }
+
+  "stateSyncing" should {
+    "updated the variable syncEnabled" in new ProfileJobsScope {
+
+      profileJobs.stateSyncing().mustRightUnit
+      profileJobs.syncEnabled shouldEqual false
+    }
+  }
+
+  "saveSharedCollection" should {
+    "returns a valid response when the process returns a right" in new ProfileJobsScope {
+
+      mockTrackEventProcess.addToMyCollectionsFromProfile(any) returns serviceRight(Unit)
+      mockDeviceProcess.getSavedApps(any)(any) returns serviceRight(seqApplicationData)
+      mockCollectionProcess.addCollection(any) returns serviceRight(collection)
+      mockProfileUiActions.showAddCollectionMessage(any) returns serviceRight(Unit)
+
+      profileJobs.saveSharedCollection(sharedCollection).mustRightUnit
+
+      there was one(mockTrackEventProcess).addToMyCollectionsFromProfile(sharedCollection.name)
+      there was one(mockProfileUiActions).showAddCollectionMessage(sharedCollection.sharedCollectionId)
+    }
+
+    "returns an AppException when the process returns an exception" in new ProfileJobsScope {
+
+      mockTrackEventProcess.addToMyCollectionsFromProfile(any) returns serviceRight(Unit)
+      mockDeviceProcess.getSavedApps(any)(any) returns serviceLeft(AppException(""))
+
+      profileJobs.saveSharedCollection(sharedCollection).mustLeft[AppException]
+
+      there was one(mockTrackEventProcess).addToMyCollectionsFromProfile(sharedCollection.name)
+      there was no(mockProfileUiActions).showAddCollectionMessage(sharedCollection.sharedCollectionId)
+    }
+
+
+    "returns an CollectionException when the process returns an exception" in new ProfileJobsScope {
+
+      mockTrackEventProcess.addToMyCollectionsFromProfile(any) returns serviceRight(Unit)
+      mockDeviceProcess.getSavedApps(any)(any) returns serviceRight(seqApplicationData)
+      mockCollectionProcess.addCollection(any) returns serviceLeft(CollectionException(""))
+
+      profileJobs.saveSharedCollection(sharedCollection).mustLeft[CollectionException]
+
+      there was one(mockTrackEventProcess).addToMyCollectionsFromProfile(sharedCollection.name)
+      there was no(mockProfileUiActions).showAddCollectionMessage(sharedCollection.sharedCollectionId)
+    }
+  }
+
+  "shareCollection" should {
+    "call to launchShare" in new ProfileJobsScope {
+
+      mockTrackEventProcess.shareCollectionFromProfile(any) returns serviceRight(Unit)
+      mockLauncherExecutorProcess.launchShare(any)(any) returns serviceRight(Unit)
+
+      profileJobs.shareCollection(sharedCollection).mustRightUnit
+
+    }
+  }
+
+  "loadPublications" should {
+    "load publications when the service returns a right response" in new ProfileJobsScope {
+
+      mockTrackEventProcess.showPublicationsContent() returns serviceRight(Unit)
+      mockProfileUiActions.showLoading() returns serviceRight(Unit)
+      mockSharedCollectionsProcess.getPublishedCollections()(any) returns serviceRight(seqSharedCollection)
+      mockProfileUiActions.loadPublications(any) returns serviceRight(Unit)
+
+      profileJobs.loadPublications().mustRightUnit
+
+      there was one(mockProfileUiActions).loadPublications(seqSharedCollection)
+    }
+
+    "call to showEmptyPublicationsContent when doesn't have sharecollections." in new ProfileJobsScope {
+
+      mockTrackEventProcess.showPublicationsContent() returns serviceRight(Unit)
+      mockProfileUiActions.showLoading() returns serviceRight(Unit)
+      mockSharedCollectionsProcess.getPublishedCollections()(any) returns serviceRight(Seq.empty)
+      mockProfileUiActions.showEmptyPublicationsContent(any) returns serviceRight(Unit)
+
+      profileJobs.loadPublications().mustRightUnit
+
+      there was one(mockProfileUiActions).showEmptyPublicationsContent(false)
+    }
+  }
+
+  "loadSubscriptions" should {
+    "load subscription when the service returns a right response" in new ProfileJobsScope {
+
+      mockTrackEventProcess.showSubscriptionsContent() returns serviceRight(Unit)
+      mockProfileUiActions.showLoading() returns serviceRight(Unit)
+      mockSharedCollectionsProcess.getSubscriptions()(any) returns serviceRight(seqSubscription)
+      mockProfileUiActions.setSubscriptionsAdapter(any) returns serviceRight(Unit)
+
+      profileJobs.loadSubscriptions().mustRightUnit
+
+      there was one(mockProfileUiActions).setSubscriptionsAdapter(seqSubscription)
+    }
+
+    "call to showEmptySubscriptionsContent when doesn't have susbcription." in new ProfileJobsScope {
+
+      mockTrackEventProcess.showSubscriptionsContent() returns serviceRight(Unit)
+      mockProfileUiActions.showLoading() returns serviceRight(Unit)
+      mockSharedCollectionsProcess.getSubscriptions()(any) returns serviceRight(Seq.empty)
+      mockProfileUiActions.showEmptySubscriptionsContent(any) returns serviceRight(Unit)
+
+      profileJobs.loadSubscriptions().mustRightUnit
+
+      there was one(mockProfileUiActions).showEmptySubscriptionsContent(false)
+    }
+  }
+
+  "changeSubscriptionStatus" should {
+    "change subscription status to Subscribe" in new ProfileJobsScope {
+
+      mockTrackEventProcess.subscribeToCollection(any) returns serviceRight(Unit)
+      mockSharedCollectionsProcess.subscribe(any)(any) returns serviceRight(Unit)
+      mockTrackEventProcess.unsubscribeFromCollection(any) returns serviceRight(Unit)
+      mockSharedCollectionsProcess.unsubscribe(any)(any) returns serviceRight(Unit)
+      mockProfileUiActions.showUpdatedSubscriptions(any,any) returns serviceRight(Unit)
+
+      profileJobs.changeSubscriptionStatus(sharedCollection.id, true).mustRightUnit
+
+      there was one(mockProfileUiActions).showUpdatedSubscriptions(sharedCollection.id, true)
+
+    }
+
+    "change subscription status to Unsubscribe" in new ProfileJobsScope {
+
+      mockTrackEventProcess.subscribeToCollection(any) returns serviceRight(Unit)
+      mockSharedCollectionsProcess.subscribe(any)(any) returns serviceRight(Unit)
+      mockTrackEventProcess.unsubscribeFromCollection(any) returns serviceRight(Unit)
+      mockSharedCollectionsProcess.unsubscribe(any)(any) returns serviceRight(Unit)
+      mockProfileUiActions.showUpdatedSubscriptions(any,any) returns serviceRight(Unit)
+
+      profileJobs.changeSubscriptionStatus(sharedCollection.id, false).mustRightUnit
+
+      there was one(mockProfileUiActions).showUpdatedSubscriptions(sharedCollection.id, false)
+
+    }
+  }
 }
--- modules/commons-tests/src/main/scala/cards/nine/commons/test/data/SharedCollectionTestData.scala
+++ modules/commons-tests/src/main/scala/cards/nine/commons/test/data/SharedCollectionTestData.scala
@@ -3,7 +3,8 @@ package cards.nine.commons.test.data
 import cards.nine.commons.test.data.CommonValues._
 import cards.nine.commons.test.data.CollectionValues._
 import cards.nine.commons.test.data.SharedCollectionValues._
-import cards.nine.models.{SharedCollection, SharedCollectionPackage}
+import cards.nine.models.types.AppCardType
+import cards.nine.models.{Subscription, SharedCollection, SharedCollectionPackage}
 
 trait SharedCollectionTestData extends CollectionTestData {
 
@@ -40,8 +41,20 @@ trait SharedCollectionTestData extends CollectionTestData {
   val publicationListIds = seqSharedCollection.map(_.sharedCollectionId)
 
   val seqPublicCollection =
-    seqCollection.flatMap(collection => collection.originalSharedCollectionId.map((_, collection))).filter{
+    seqCollection.flatMap(collection => collection.originalSharedCollectionId.map((_, collection))).filter {
       case (sharedCollectionId: String, _) => !publicationListIds.contains(sharedCollectionId)
     }
+
+  def subscription(num: Int = 0) = Subscription(
+    id = collection.id + num,
+    sharedCollectionId = sharedCollectionId + num,
+    name = collection.name,
+    apps = collection.cards.count(card => card.cardType == AppCardType),
+    icon = collection.icon,
+    themedColorIndex = collection.themedColorIndex,
+    subscribed = collection.sharedCollectionSubscribed)
+
+  val subscription: Subscription = subscription(0)
+  val seqSubscription: Seq[Subscription] = Seq(subscription(0), subscription(1), subscription(2))
 
 }
