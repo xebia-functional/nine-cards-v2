69bc41fe6a068f00c0f667f2702246ca70cd619f
Javi Pacheco <javi@47deg.com>
2016-12-01 12:24:19 +0100

    Upgraded FlowUp and sbt android plugin

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,6 @@ object Versions {
   val macroidV = "2.0-SNAPSHOT"
   val playJsonV = "2.3.7"
   val specs2V = "2.4.15"
-  val picassoV = "2.5.0"
   val playServicesV = "9.6.1"
   val mockitoV = "3.0-M2"
   val glideV = "3.7.0"
@@ -24,5 +23,5 @@ object Versions {
   val gfcTimeUUIDV = "0.0.6"
   val monixV = "2.1.1"
   val catsV = "0.8.1"
-  val flowUpV= "0.1.8"
+  val flowUpV= "0.2.4"
 }
