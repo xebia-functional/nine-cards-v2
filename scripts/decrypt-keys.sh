ebe66903282102b2a0bff6d921be4aa41faad4f9
Javi Pacheco <javi@47deg.com>
2017-01-18 11:51:49 +0100

    Script fixed

--- scripts/decrypt-keys.sh
+++ scripts/decrypt-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-openssl openssl aes-256-cbc -K $encrypted_0085ba44c983_key -iv $encrypted_0085ba44c983_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
+openssl aes-256-cbc -K $encrypted_0085ba44c983_key -iv $encrypted_0085ba44c983_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
 chmod 600 travis-deploy-key;
 cp travis-deploy-key ~/.ssh/id_rsa;
